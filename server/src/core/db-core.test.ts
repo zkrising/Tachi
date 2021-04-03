ae182b9f811c74db35e7116556e7e194cabfc07b
zkldi <ddhitone@gmail.com>
2021-04-03 21:32:28 +0100

    Fix and statically type dbcoretest

--- server/src/core/db-core.test.ts
+++ server/src/core/db-core.test.ts
@@ -1,6 +1,7 @@
 import { GetNextCounterValue } from "./db-core";
 import t from "tap";
 import db from "../db";
+import { CounterDocument } from "kamaitachi-common";
 
 t.test("#GetNextCounterValue", async (t) => {
     t.test("Increments on valid counter hit", async (t) => {
@@ -9,11 +10,11 @@ t.test("#GetNextCounterValue", async (t) => {
         // database starts with this at one
         t.is(response, 2, "Counter should return an integer");
 
-        let dbData = await db.get("counters").findOne({
+        let dbData = await db.get<CounterDocument>("counters").findOne({
             counterName: "real-counter",
         });
 
-        t.is(dbData.count, 3, "Counter should increment after being hit");
+        t.is(dbData.value, 3, "Counter should increment after being hit");
     });
 
     t.throws(

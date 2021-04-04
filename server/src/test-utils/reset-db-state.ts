af11ae0b0f7e461e389d6635ec19fb669baabcf2
zkldi <ddhitone@gmail.com>
2021-04-04 17:58:22 +0100

    factor out async in reset-db-state

--- server/src/test-utils/reset-db-state.ts
+++ server/src/test-utils/reset-db-state.ts
@@ -7,21 +7,29 @@ import path from "path";
 
 const DATA_DIR = path.join(__dirname, "./mock-db");
 
+async function ResetState(file: string) {
+    let filename = path.basename(file, ".json");
+
+    await db.get(filename).remove({});
+
+    let fileLoc = path.join(DATA_DIR, file);
+    let data = JSON.parse(fs.readFileSync(fileLoc, "utf-8"));
+
+    if (!Array.isArray(data)) {
+        throw new Error(`Panic, ${filename} not JSONArray?`);
+    }
+
+    await db.get(filename).insert(data);
+}
+
 export default async function ResetDBState() {
     const files = fs.readdirSync(DATA_DIR);
 
+    const promises = [];
+
     for (const file of files) {
-        let filename = path.basename(file, ".json");
-
-        await db.get(filename).remove({});
-
-        let fileLoc = path.join(DATA_DIR, file);
-        let data = JSON.parse(fs.readFileSync(fileLoc).toString());
-
-        if (!Array.isArray(data)) {
-            throw new Error(`Panic, ${filename} not JSONArray?`);
-        }
-
-        await db.get(filename).insert(data);
+        promises.push(ResetState(file));
     }
+
+    await Promise.all(promises);
 }

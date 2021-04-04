d9d8380304fb4747690ab2c939adfec61a0a2410
zkldi <ddhitone@gmail.com>
2021-04-04 17:56:40 +0100

    Inject properties into express-session

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,1 +1,13 @@
 export type integer = number;
+
+declare module "express-session" {
+    // Inject additional properties on express-session
+    interface SessionData {
+        ktchi: KtchiSessionData;
+    }
+}
+
+interface KtchiSessionData {
+    userID: integer;
+    apiKey: string;
+}

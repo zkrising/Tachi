00062e43b7f247441b6ce40847c08a6b0360a876
zkldi <ddhitone@gmail.com>
2021-04-04 17:57:12 +0100

    fix nonsense in db-core

--- server/src/core/db-core.test.ts
+++ server/src/core/db-core.test.ts
@@ -1,26 +1,31 @@
 import { GetNextCounterValue } from "./db-core";
 import t from "tap";
-import db from "../db";
+import db, { CloseConnection } from "../db";
 import { CounterDocument } from "kamaitachi-common";
+import ResetDBState from "../test-utils/reset-db-state";
 
-t.test("#GetNextCounterValue", async (t) => {
+t.test("#GetNextCounterValue", (t) => {
+    t.beforeEach(ResetDBState);
+
     t.test("Increments on valid counter hit", async (t) => {
         let response = await GetNextCounterValue("real-counter");
 
         // database starts with this at one
-        t.is(response, 2, "Counter should return an integer");
+        t.is(response, 2, "Counter should return the current number stored");
 
         let dbData = await db.get<CounterDocument>("counters").findOne({
             counterName: "real-counter",
         });
 
-        t.is(dbData.value, 3, "Counter should increment after being hit");
+        t.is(dbData!.value, 3, "Counter should increment after being hit");
     });
 
-    t.throws(
+    t.rejects(
         async () => await GetNextCounterValue("fake-counter"),
         "Could not find sequence document for fake-counter."
     );
 
     t.end();
 });
+
+t.teardown(CloseConnection);

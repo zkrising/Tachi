302158105b716d9afcd696e5bd005c8de5083543
zkldi <notzkldi@zkldi.com>
2021-04-19 00:56:08 +0100

    remove supportedFileUploads

--- server/src/server-config.ts
+++ server/src/server-config.ts
@@ -1,7 +1,3 @@
-// we'll see how this works?
-const supportedFileUploads = ["iidx:eamusement-csv", "iidx:plife-csv", "any:batchmanual-json"];
-
 export default {
     PORT: process.env.PORT ?? 8080,
-    supportedFileUploads,
 };

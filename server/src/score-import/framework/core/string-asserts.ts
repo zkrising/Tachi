4e195634d6d8f335f14144819f191b663b90fa80
zkldi <notzkldi@zkldi.com>
2021-04-20 16:29:38 +0100

    useful asserts for parser

---
+++ server/src/score-import/framework/core/string-asserts.ts
@@ -0,0 +1,43 @@
+import { InvalidScoreFailure } from "./converter-errors";
+
+const isIntegerRegex = /^-?\d+$/;
+
+export function AssertStrAsPositiveInt(strVal: string, errorMessage: string) {
+    let isInt = isIntegerRegex.test(strVal);
+
+    if (!isInt) {
+        throw new InvalidScoreFailure(`${errorMessage} (Not an integer.)`);
+    }
+
+    let val = parseInt(strVal, 10);
+
+    if (Number.isNaN(val)) {
+        throw new InvalidScoreFailure(`${errorMessage} (Not a number.)`);
+    } else if (!Number.isSafeInteger(val)) {
+        throw new InvalidScoreFailure(`${errorMessage} (Not an integer.)`);
+    } else if (val < 0) {
+        throw new InvalidScoreFailure(`${errorMessage} (Was Negative).`);
+    }
+
+    return val;
+}
+
+export function AssertStrAsPositiveNonZeroInt(strVal: string, errorMessage: string) {
+    let isInt = isIntegerRegex.test(strVal);
+
+    if (!isInt) {
+        throw new InvalidScoreFailure(`${errorMessage} (Not an integer.)`);
+    }
+
+    let val = parseInt(strVal, 10);
+
+    if (Number.isNaN(val)) {
+        throw new InvalidScoreFailure(`${errorMessage} (Not a number.)`);
+    } else if (!Number.isSafeInteger(val)) {
+        throw new InvalidScoreFailure(`${errorMessage} (Not an integer.)`);
+    } else if (val <= 0) {
+        throw new InvalidScoreFailure(`${errorMessage} (Was Negative).`);
+    }
+
+    return val;
+}

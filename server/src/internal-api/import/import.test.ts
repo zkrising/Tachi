1a0ad16868a82f35ce4855e01cd394cea66af8ff
zkldi <notzkldi@zkldi.com>
2021-04-20 16:29:12 +0100

    the goal is to make this not hang

--- server/src/internal-api/import/import.test.ts
+++ server/src/internal-api/import/import.test.ts
@@ -4,23 +4,42 @@ import { TestingIIDXEamusementCSV26 } from "../../test-utils/test-data";
 import { CloseAllConnections } from "../../test-utils/close-connections";
 import { RequireNeutralAuthentication } from "../../test-utils/api-common";
 import { CreateFakeAuthCookie } from "../../test-utils/fake-session";
+import test from "why-is-node-running";
 import ResetDBState from "../../test-utils/reset-db-state";
+import { rootLogger } from "../../logger";
 
 t.test("POST /internal-api/import/file", async (t) => {
+    const cookie = await CreateFakeAuthCookie(mockApi);
+    rootLogger.info("start t.test");
     t.beforeEach(ResetDBState);
-    const cookie = await CreateFakeAuthCookie();
-    function PostImportFile() {
-        return mockApi.post("/internal-api/import/file").set("Cookie", cookie);
-    }
 
-    RequireNeutralAuthentication(t, "/internal-api/import/file", "POST");
+    rootLogger.info("start neutral auth");
+
+    t.test(`Testing authentication for POST /internal-api/import/file`, async (t) => {
+        let res = await mockApi.post("/internal-api/import/file");
+
+        t.equal(res.status, 401, "Should return 401 immediately.");
+        t.equal(
+            res.body.description,
+            "You are not authorised to perform this action.",
+            "Should return an appropriate error message."
+        );
+
+        rootLogger.debug("end neutral auth");
+        t.end();
+    });
 
     t.test("csv:eamusement-iidx", (t) => {
-        t.test("Successful File Upload", async (t) => {
-            let res = await PostImportFile()
+        t.test("Valid Rootage CSV import", async (t) => {
+            rootLogger.info("import 1");
+            let res = await mockApi
+                .post("/internal-api/import/file")
+                .set("Cookie", cookie)
                 .attach("scoreData", TestingIIDXEamusementCSV26, "my_csv.csv")
                 .field("importType", "csv:eamusement-iidx")
                 .field("playtype", "SP");
+
+            rootLogger.info("import 2");
 
             t.strictSame(
                 res.body,
@@ -31,6 +50,7 @@ t.test("POST /internal-api/import/file", async (t) => {
                 "Should return information of a success."
             );
 
+            rootLogger.info("import 3");
             t.end();
         });
 
@@ -39,5 +59,9 @@ t.test("POST /internal-api/import/file", async (t) => {
 
     t.end();
 });
+
+setTimeout(() => {
+    test();
+}, 10000);
 
 t.teardown(CloseAllConnections);

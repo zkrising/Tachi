b7ce6138f7d6a97f0e0655c0ea37e89cca1421e0
zkldi <notzkldi@zkldi.com>
2021-04-25 01:52:05 +0100

    add redis critical events

--- server/src/redis/redis-store.ts
+++ server/src/redis/redis-store.ts
@@ -11,6 +11,20 @@ logger.info("Instantiated Redis Store");
 export const RedisClient = redis.createClient();
 logger.info("Instantiated Redis Client");
 
+function EmitCritical() {
+    if (!RedisClient.connected) {
+        logger.crit(`Could not connect to redis in time. No more information is available.`);
+    }
+}
+
+let ref = setTimeout(EmitCritical, 10000);
+
+RedisClient.on("connect", () => {
+    logger.info(`Connected to Redis.`);
+
+    clearTimeout(ref);
+});
+
 export function CloseRedisConnection() {
     return RedisClient.quit();
 }

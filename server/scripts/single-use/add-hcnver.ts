5244521750a75d635590e944af1ff2754ccee7cf
zkldi <notzkldi@zkldi.com>
2021-04-28 03:22:31 +0100

    change up add-hcnver to, yknow, work

--- server/scripts/single-use/add-hcnver.ts
+++ server/scripts/single-use/add-hcnver.ts
@@ -1,21 +1,24 @@
-/* eslint-disable no-console */
 import fs from "fs";
 import path from "path";
 import { PRUDENCE_CHART_SCHEMAS } from "../../src/db/schemas";
 import p from "prudence";
 import db from "../../src/db/db";
+import CreateLogCtx from "../../src/logger";
+
+const logger = CreateLogCtx("add-hcnver.ts");
 
 let data = JSON.parse(fs.readFileSync(path.join(__dirname, "./hcnverdata.json"), "utf-8"));
 
 for (const d of data) {
     let r = p(d, PRUDENCE_CHART_SCHEMAS.iidx);
     if (r) {
-        console.dir(r);
+        logger.error(r.userVal);
+        logger.error(r);
         throw r;
     }
 }
 
 db.charts.iidx.insert(data).then(() => {
-    console.log("Done.");
+    logger.info("Done.");
     process.exit(0);
 });

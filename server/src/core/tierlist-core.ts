369754cba0bcca891f8d2111730845807276d84e
zkldi <notzkldi@zkldi.com>
2021-05-06 02:50:57 +0100

    rating core tierlist support???

--- server/src/core/tierlist-core.ts
+++ server/src/core/tierlist-core.ts
@@ -1,4 +1,9 @@
 import crypto from "crypto";
+import { AnyChartDocument, Game, Playtypes } from "kamaitachi-common";
+import db from "../db/db";
+import CreateLogCtx from "../logger";
+
+const logger = CreateLogCtx("tierlist-core.ts");
 
 export function CalculateTierlistDataID(
     chartID: string,
@@ -10,4 +15,61 @@ export function CalculateTierlistDataID(
         .createHash("sha256")
         .update(`${chartID}|${type}|${key}|${tierlistID}`)
         .digest("hex");
+}
+
+export function GetDefaultTierlist(game: Game, playtype: Playtypes[Game]) {
+    return db.tierlists.findOne({
+        game,
+        playtype,
+        isDefault: true,
+    });
+}
+
+async function GetDefaultTierlistID(game: Game, playtype: Playtypes[Game]) {
+    let tierlistData = await GetDefaultTierlist(game, playtype);
+
+    if (!tierlistData) {
+        logger.warn(
+            `Attempted to GetTierlistData for ${game} ${playtype}, but no default tierlist is set.`
+        );
+        return undefined;
+    }
+
+    return tierlistData.tierlistID;
+}
+
+export async function GetOneTierlistData(
+    game: Game,
+    chart: AnyChartDocument,
+    type: "score" | "lamp" | "grade",
+    key: string | null,
+    tierlistID?: string
+) {
+    let tlID = tierlistID ?? (await GetDefaultTierlistID(game, chart.playtype));
+
+    let query: Record<string, unknown> = {
+        tierlistID: tlID,
+        chartID: chart.chartID,
+        type,
+        key,
+    };
+
+    return db["tierlist-data"].findOne(query);
+}
+
+export async function GetAllTierlistDataOfType(
+    game: Game,
+    chart: AnyChartDocument,
+    type: "score" | "lamp" | "grade",
+    tierlistID?: string
+) {
+    let tlID = tierlistID ?? (await GetDefaultTierlistID(game, chart.playtype));
+
+    let query: Record<string, unknown> = {
+        tierlistID: tlID,
+        chartID: chart.chartID,
+        type,
+    };
+
+    return db["tierlist-data"].find(query);
 }
--- server/src/score-import/framework/calculated-data/calculated-data.ts
+++ server/src/score-import/framework/calculated-data/calculated-data.ts
@@ -1,5 +1,11 @@
 import { AnyChartDocument, Game, Playtypes, ScoreDocument } from "kamaitachi-common";
+import { ratingParameters, lamps, clearLamp } from "kamaitachi-common/js/config";
 import { Logger } from "winston";
+import {
+    GetAllTierlistDataOfType,
+    GetDefaultTierlist,
+    GetOneTierlistData,
+} from "../../../core/tierlist-core";
 import { DryScore } from "../../../types";
 import { CreateGameSpecific } from "./game-specific";
 import { CalculateCHUNITHMRating, CalculateGITADORARating } from "./game-specific-stats";
@@ -12,14 +18,18 @@ export async function CreateCalculatedData(
     const game = dryScore.game;
     const playtype = chart.playtype;
 
-    const [rating, gameSpecific] = await Promise.all([
-        CalculateRating(dryScore, game, playtype, chart),
+    let defaultTierlist = await GetDefaultTierlist(game, playtype);
+    let defaultTierlistID = defaultTierlist?.tierlistID; // tierlistID | undefined
+
+    const [rating, lampRating, gameSpecific] = await Promise.all([
+        CalculateRating(dryScore, game, playtype, chart, defaultTierlistID),
+        CalculateLampRating(dryScore, game, playtype, chart, defaultTierlistID),
         CreateGameSpecific(game, playtype, chart, dryScore, logger),
     ]);
 
     return {
         rating,
-        lampRating: 0,
+        lampRating,
         gameSpecific,
     };
 }
@@ -39,34 +49,141 @@ const OVERRIDE_RATING_FUNCTIONS: Partial<Record<Game, any>> = {
     },
 };
 
+interface RatingParameters {
+    failHarshnessMultiplier: number;
+    pivotPercent: number;
+    clearExpMultiplier: number;
+}
+
 /**
  * Calculates the rating for a score. Listens to the override functions declared above.
  */
-function CalculateRating<G extends Game>(
+async function CalculateRating(
     dryScore: DryScore,
-    game: G,
-    playtype: Playtypes[G],
-    chart: AnyChartDocument
+    game: Game,
+    playtype: Playtypes[Game],
+    chart: AnyChartDocument,
+    defaultTierlistID?: string
 ) {
     // @todo
     let OverrideFunction: OverrideRatingFunction | undefined =
         OVERRIDE_RATING_FUNCTIONS[game]?.[playtype];
 
+    // If this game doesn't have a specific rating function declared, fall back to the default "generic" rating function.
+    // This is just a function that is guaranteed to work for all input - and therefore not result in lower-skilled users
+    // always seeing 0.
     if (!OverrideFunction) {
-        return 0; // go to default kamaitachi rating fn ?
+        let parameters = ratingParameters[dryScore.game];
+
+        let levelNum = chart.levelNum;
+
+        if (defaultTierlistID) {
+            let tierlistData = await GetOneTierlistData(
+                game,
+                chart,
+                "score",
+                null,
+                defaultTierlistID
+            );
+
+            if (tierlistData) {
+                levelNum = tierlistData.data.value;
+            }
+        }
+
+        return RatingCalcV1(dryScore.scoreData.percent, levelNum, parameters);
     }
 
     return OverrideFunction(dryScore, chart);
 }
 
-// stub
-// async function CalculateLampRating(
-//     dryScore: DryScore,
-//     chart: ChartDocument,
-//     tierlist: TierlistDataDocument
-// ) {
-//     return 0;
-// }
+// Generic Rating Calc that is guaranteed to work for everything. This is unspecialised, and not great.
+function RatingCalcV1(percent: number, levelNum: number, parameters: RatingParameters) {
+    if (percent > parameters.pivotPercent) {
+        return RatingCalcV0Fail(percent, levelNum, parameters);
+    }
+
+    return RatingCalcV1Clear(percent, levelNum, parameters);
+}
+
+function RatingCalcV1Clear(percent: number, levelNum: number, parameters: RatingParameters) {
+    // https://www.desmos.com/calculator/hn7uxjmjkc
+
+    let rating =
+        Math.cosh(parameters.clearExpMultiplier * levelNum * (percent - parameters.pivotPercent)) +
+        (levelNum - 1);
+    if (!isFinite(rating)) {
+        return 0;
+    } else if (Number.isNaN(rating)) {
+        return 0; // safety checks
+    } else if (rating > 1000) {
+        return 0; // AAAAA JUST STOP THE WEIRD STUFF
+    } else {
+        return rating;
+    }
+}
+
+function RatingCalcV0Fail(percent: number, levelNum: number, parameters: RatingParameters) {
+    // https://www.desmos.com/calculator/ngjie5elto
+    return (
+        percent ** (parameters.failHarshnessMultiplier * levelNum) *
+        (levelNum / parameters.pivotPercent ** (parameters.failHarshnessMultiplier * levelNum))
+    );
+}
+
+async function CalculateLampRating(
+    dryScore: DryScore,
+    game: Game,
+    playtype: Playtypes[Game],
+    chart: AnyChartDocument,
+    defaultTierlistID?: string
+) {
+    // if no tierlist data
+    if (!defaultTierlistID) {
+        return LampRatingNoTierlistInfo(dryScore, game, chart);
+    }
+
+    let lampTierlistInfo = await GetAllTierlistDataOfType(game, chart, "lamp", defaultTierlistID);
+
+    // if no tierlist info
+    if (lampTierlistInfo.length === 0) {
+        return LampRatingNoTierlistInfo(dryScore, game, chart);
+    }
+
+    let userLampIndex = lamps[game].indexOf(dryScore.scoreData.lamp);
+
+    let lampRating = 0;
+
+    // why is this like this and not a lookup table?
+    // Some charts have higher values for *lower* lamps, such as
+    // one more lovely in IIDX being harder to NC than it is to HC!
+    // this means we have to iterate over all tierlist data, in general.
+    for (const tierlistData of lampTierlistInfo) {
+        if (
+            // this tierlistData has higher value than the current lampRating
+            tierlistData.data.value > lampRating &&
+            // and your clear is better than the lamp its for
+            lamps[game].indexOf(tierlistData.key!) <= userLampIndex
+        ) {
+            lampRating = tierlistData.data.value;
+        }
+    }
+
+    return lampRating;
+}
+
+function LampRatingNoTierlistInfo(dryScore: DryScore, game: Game, chart: AnyChartDocument) {
+    const CLEAR_LAMP_INDEX = lamps[game].indexOf(clearLamp[game]);
+
+    // if this is a clear
+    if (lamps[game].indexOf(dryScore.scoreData.lamp) >= CLEAR_LAMP_INDEX) {
+        // return this chart's numeric level as the lamp rating
+        return chart.levelNum;
+    }
+
+    // else, this score is worth 0.
+    return 0;
+}
 
 interface OverrideRatingFunction {
     (dryScore: DryScore, chartData: AnyChartDocument): number | Promise<number>;

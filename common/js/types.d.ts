0e099967121afb75c55c43814acc0f8c32848c56
zkldi <notzkldi@zkldi.com>
2021-05-12 01:29:55 +0100

    oh i beat the compiler

--- common/js/types.d.ts
+++ common/js/types.d.ts
@@ -965,7 +965,7 @@ export interface PBScoreDocument<I extends IDStrings = IDStrings> extends MongoD
         lampIndex: integer;
         gradeIndex: integer;
         esd: number | null;
-        hitData: Partial<Record<JudgementLookup[I], integer>>;
+        hitData: Partial<Record<JudgementLookup[I], integer | null>>;
         hitMeta: Partial<HitMetaLookup[I]>;
     };
     calculatedData: {

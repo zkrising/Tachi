23285d36d8c426a37f63a7da62272a6299675da0
zkldi <notzkldi@zkldi.com>
2021-05-13 22:26:21 +0100

    mount fervidex

--- server/src/api/ir/ir.ts
+++ server/src/api/ir/ir.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import directManualIR from "./direct-manual/direct-manual";
 import barbatosIR from "./barbatos/barbatos";
+import fervidexIR from "./fervidex/fervidex";
 
 const router: Router = Router({ mergeParams: true });
 
 router.use("/direct-manual", directManualIR);
 router.use("/barbatos", barbatosIR);
+router.use("/fervidex", fervidexIR);
 
 export default router;

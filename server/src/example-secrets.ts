9b8388ef885a1c3046a1a063a3fdd4636b303ca6
zkldi <notzkldi@zkldi.com>
2021-05-20 08:33:06 +0100

    add example-secrets file for future maintainers

---
+++ server/src/example-secrets.ts
@@ -0,0 +1,11 @@
+// This is an example file for the secrets.ts file - which is not not available in the repository.
+
+export const CAPTCHA_SECRET_KEY = "change-me";
+
+export const SESSION_SECRET = "change-me";
+
+export const FLO_API_URL = "https://google.com";
+
+export const EAG_API_URL = "https://google.com";
+
+export const ARC_API_URL = "https://google.com";

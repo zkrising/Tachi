fe42814f967c9eed28b2b715fd78a0219e74b454
zkldi <notzkldi@zkldi.com>
2021-05-20 07:30:46 +0100

    generic kai-sdvx parser

---
+++ server/src/score-import/import-types/common/api-kai/sdvx/parser.ts
@@ -0,0 +1,26 @@
+import { KtLogger, ParserFunctionReturnsAsync } from "../../../../../types";
+import nodeFetch from "../../../../../fetch";
+import { KaiAuthDocument } from "kamaitachi-common";
+import { KaiContext } from "../types";
+import { EAG_API_URL, FLO_API_URL } from "../../../../../secrets";
+import { ConvertAPIKaiSDVX } from "./converter";
+import { TraverseKaiAPI } from "../traverse-api";
+
+export function ParseKaiSDVX(
+    service: "FLO" | "EAG",
+    authDoc: KaiAuthDocument,
+    logger: KtLogger,
+    fetch = nodeFetch
+): ParserFunctionReturnsAsync<unknown, KaiContext> {
+    const baseUrl = service === "FLO" ? FLO_API_URL : EAG_API_URL;
+
+    return {
+        iterable: TraverseKaiAPI(baseUrl, "/api/sdvx/v1/play_history", authDoc, logger, fetch),
+        context: {
+            service,
+        },
+        classHandler: null,
+        game: "iidx",
+        ConverterFunction: ConvertAPIKaiSDVX,
+    };
+}

9fb7698d87e545dd95cffc8bb0adcca6fa5935ec
zkldi <notzkldi@zkldi.com>
2021-05-20 07:32:32 +0100

    api/eag-iidx, api/eag-sdvx, api/flo-iidx, api/flo-sdvx

---
+++ server/src/score-import/import-types/api/flo-iidx/parser.ts
@@ -0,0 +1,7 @@
+import { KtLogger } from "../../../../types";
+import { ParseKaiIIDX } from "../../common/api-kai/iidx/parser";
+import { KaiAuthDocument } from "kamaitachi-common";
+
+export function ParseFloIIDX(authDoc: KaiAuthDocument, logger: KtLogger) {
+    return ParseKaiIIDX("FLO", authDoc, logger);
+}

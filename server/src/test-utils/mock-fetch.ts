d9b396cd7af714f19b7d74c26cda6fde68fdf337
zkldi <notzkldi@zkldi.com>
2021-05-20 08:07:32 +0100

    fix invalid typing in mock-fetch

--- server/src/test-utils/mock-fetch.ts
+++ server/src/test-utils/mock-fetch.ts
@@ -1,7 +1,8 @@
-import nodeFetch, { Response } from "node-fetch";
+import { Response } from "node-fetch";
+import { NodeFetch } from "../fetch";
 
 export function MockFetch(data: Partial<Response>) {
     // We know! this is a mock function.
     // eslint-disable-next-line require-await
-    return ((async () => data) as unknown) as typeof nodeFetch;
+    return ((async () => data) as unknown) as NodeFetch;
 }

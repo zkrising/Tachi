0712f8ca3f093f37abb3aaeb253d63d5162cf395
zkldi <notzkldi@zkldi.com>
2021-05-24 17:10:10 +0100

    add a stat script for checks

---
+++ server/scripts/single-use/fix-iidx-versions/stat.ts
@@ -0,0 +1,20 @@
+import CreateLogCtx from "../../../src/common/logger";
+import db from "../../../src/db/db";
+
+const logger = CreateLogCtx("scripts/single-use/fix-iidx-versions/stat.ts");
+
+(async () => {
+    const charts = await db.charts.iidx.find({ versions: { $size: 0 } });
+
+    for (const chart of charts) {
+        // eslint-disable-next-line no-await-in-loop
+        const song = await db.songs.iidx.findOne({ id: chart.songID });
+
+        if (!song) {
+            logger.error(`cannot find parent song ${chart.songID}`);
+            continue;
+        }
+
+        logger.info(`${song.title} ${chart.playtype} ${chart.difficulty}`);
+    }
+})();

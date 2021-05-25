cfa0b92e2df776dbbafda7a736fc91f3c45dcd32
zkldi <notzkldi@zkldi.com>
2021-05-25 20:28:07 +0100

    remove secrets and replace it with env barrel

---
+++ server/src/lib/env/env.ts
@@ -0,0 +1,25 @@
+// barrel file for re-exporting env variables.
+
+const envVars = [
+    "KTBKS_MONGO_BASE_URL",
+    "KTBKS_LOG_LEVEL",
+    "KTBKS_CAPTCHA_SECRET_KEY",
+    "KTBKS_SESSION_SECRET",
+    "KTBKS_FLO_API_URL",
+    "KTBKS_EAG_API_URL",
+    "KTBKS_ARC_API_URL",
+];
+
+for (const eVar of envVars) {
+    if (!process.env[eVar]) {
+        throw new Error(`${eVar} was not defined.`);
+    }
+}
+
+export const MONGO_BASE_URL = process.env.KTBKS_MONGO_BASE_URL!;
+export const LOG_LEVEL = process.env.KTBKS_LOG_LEVEL!;
+export const SESSION_SECRET = process.env.KTBKS_SESSION_SECRET!;
+export const CAPTCHA_SECRET_KEY = process.env.KTBKS_CAPTCHA_SECRET_KEY!;
+export const FLO_API_URL = process.env.KTBKS_FLO_API_URL!;
+export const EAG_API_URL = process.env.KTBKS_EAG_API_URL!;
+export const ARC_API_URL = process.env.KTBKS_ARC_API_URL!;

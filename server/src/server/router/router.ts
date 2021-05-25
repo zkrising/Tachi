6fb1fff6c23cba2584b6e9ca3752c1aaee6c0cef
zkldi <notzkldi@zkldi.com>
2021-05-25 19:45:20 +0100

    Git got stuck

--- server/src/server/router/router.ts
+++ server/src/server/router/router.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import apiRouter from "./api/router";
+import apiRouterV1 from "./api/v1/router";
 import irRouter from "./ir/router";
 
 const router: Router = Router({ mergeParams: true });
 
-router.use("/api/v1", apiRouter);
+router.use("/api/v1", apiRouterV1);
 router.use("/ir", irRouter);
 
 export default router;

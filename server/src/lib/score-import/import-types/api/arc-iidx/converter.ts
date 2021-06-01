21875665d456351f460f08c404a21a1af35b6d73
zkldi <notzkldi@zkldi.com>
2021-06-01 21:23:21 +0100

    api/arc-iidx support

---
+++ server/src/lib/score-import/import-types/api/arc-iidx/converter.ts
@@ -0,0 +1,114 @@
+import { EmptyObject } from "../../../../../utils/types";
+import { ConverterFunction } from "../../common/types";
+import p, { PrudenceSchema } from "prudence";
+import {
+    InvalidScoreFailure,
+    KTDataNotFoundFailure,
+} from "../../../framework/common/converter-failures";
+import { FormatPrError } from "../../../../../utils/prudence";
+import { ARCIIDXScore } from "./types";
+import { FindChartOnARCID } from "../../../../../utils/queries/charts";
+import { FindSongOnIDGuaranteed } from "../../../../../utils/queries/songs";
+import { DryScore } from "../../../framework/common/types";
+import {
+    GenericGetGradeAndPercent,
+    ParseDateFromString,
+} from "../../../framework/common/score-utils";
+import { Lamps } from "kamaitachi-common";
+
+// There's a bunch of other useless fields but we don't care
+const PR_ArcIIDXScore: PrudenceSchema = {
+    chart_id: "string",
+    lamp: p.isIn(
+        "FULL_COMBO",
+        "EX_HARD_CLEAR",
+        "HARD_CLEAR",
+        "CLEAR",
+        "EASY_CLEAR",
+        "ASSIST_CLEAR",
+        "FAILED",
+        "NO_PLAY"
+    ),
+    ex_score: p.isPositiveInteger,
+    miss_count: p.nullable(p.isPositiveInteger),
+    timestamp: "string",
+};
+
+export const ConvertAPIArcIIDX: ConverterFunction<unknown, EmptyObject> = async (
+    data,
+    context,
+    importType,
+    logger
+) => {
+    const err = p(data, PR_ArcIIDXScore, {}, { throwOnNonObject: false, allowExcessKeys: true });
+
+    if (err) {
+        throw new InvalidScoreFailure(FormatPrError(err, "Invalid ARC Score: "));
+    }
+
+    // confirmed by Prudence above.
+    const score = data as ARCIIDXScore;
+
+    const chart = await FindChartOnARCID("iidx", score.chart_id);
+
+    if (!chart) {
+        throw new KTDataNotFoundFailure(
+            `Could not find chart with chart_id ${score.chart_id}.`,
+            importType,
+            data,
+            context
+        );
+    }
+
+    const song = await FindSongOnIDGuaranteed("iidx", chart.songID, logger);
+
+    const { grade, percent } = GenericGetGradeAndPercent("iidx", score.ex_score, chart);
+
+    const timeAchieved = ParseDateFromString(score.timestamp);
+
+    const lamp = ResolveARCIIDXLamp(score.lamp);
+
+    const dryScore: DryScore<"iidx:SP" | "iidx:DP"> = {
+        comment: null,
+        game: "iidx",
+        importType,
+        timeAchieved,
+        service: "ARC IIDX27",
+        scoreData: {
+            grade,
+            percent,
+            score: score.ex_score,
+            hitData: {},
+            hitMeta: {
+                bp: score.miss_count,
+            },
+            lamp,
+        },
+        scoreMeta: {},
+    };
+
+    return { song, chart, dryScore };
+};
+
+function ResolveARCIIDXLamp(lamp: ARCIIDXScore["lamp"]): Lamps["iidx:SP" | "iidx:DP"] {
+    switch (lamp) {
+        case "NO_PLAY":
+            return "NO PLAY";
+        case "FAILED":
+            return "FAILED";
+        case "ASSIST_CLEAR":
+            return "ASSIST CLEAR";
+        case "EASY_CLEAR":
+            return "EASY CLEAR";
+        case "CLEAR":
+            return "CLEAR";
+        case "HARD_CLEAR":
+            return "HARD CLEAR";
+        case "EX_HARD_CLEAR":
+            return "EX HARD CLEAR";
+        case "FULL_COMBO":
+            return "FULL COMBO";
+    }
+
+    throw new InvalidScoreFailure(`Invalid lamp ${lamp} - Could not resolve.`);
+}
---
+++ server/src/lib/score-import/import-types/api/arc-iidx/parser.ts
@@ -0,0 +1,26 @@
+import { KtLogger } from "../../../../logger/logger";
+import nodeFetch from "../../../../../utils/fetch";
+import { TraverseKaiAPI } from "../../common/api-kai/traverse-api";
+import { ParserFunctionReturnsAsync } from "../../common/types";
+import { EmptyObject } from "../../../../../utils/types";
+import { ARC_API_URL, ARC_AUTH_TOKEN } from "../../../../env/env";
+
+export function ParseArcIIDX(
+    arcProfileID: string,
+    logger: KtLogger,
+    fetch = nodeFetch
+): ParserFunctionReturnsAsync<unknown, EmptyObject> {
+    return {
+        iterable: TraverseKaiAPI(
+            ARC_API_URL,
+            // HEROIC VERSE
+            `/api/v1/iidx/27/player_bests?profile_id=${arcProfileID}`,
+            ARC_AUTH_TOKEN,
+            logger,
+            fetch
+        ),
+        context: {},
+        classHandler: null,
+        game: "iidx",
+    };
+}

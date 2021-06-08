71232a63fb47f6155a9339b7b8ef668b6fd76922
zkldi <notzkldi@zkldi.com>
2021-06-08 17:23:42 +0100

    restrict certain IRs depending on kamai/boku

--- server/src/server/router/ir/router.ts
+++ server/src/server/router/ir/router.ts
@@ -5,14 +5,23 @@ import directManualIR from "./direct-manual/router";
 import fervidexIR from "./fervidex/router";
 import uscIR from "./usc/router";
 import beatorajaIR from "./beatoraja/router";
+import { RequireBokutachi, RequireKamaitachi } from "../../middleware/type-require";
 
 const router: Router = Router({ mergeParams: true });
 
-router.use("/barbatos", barbatosIR);
-router.use("/chunitachi", chunitachiIR);
+// Common IRs
+
 router.use("/direct-manual", directManualIR);
-router.use("/fervidex", fervidexIR);
-router.use("/usc", uscIR);
-router.use("/beatoraja", beatorajaIR);
+
+// Bokutachi IRs
+
+router.use("/usc", RequireBokutachi, uscIR);
+router.use("/beatoraja", RequireBokutachi, beatorajaIR);
+
+// Kamaitachi IRs
+
+router.use("/barbatos", RequireKamaitachi, barbatosIR);
+router.use("/chunitachi", RequireKamaitachi, chunitachiIR);
+router.use("/fervidex", RequireKamaitachi, fervidexIR);
 
 export default router;

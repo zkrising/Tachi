b604fa575d7b840c32f6c20ca20687ab08ad3f10
zkldi <notzkldi@zkldi.com>
2021-06-09 13:00:15 +0100

    Experimental rate limit tests

--- server/src/server/router/api/v1/router.ts
+++ server/src/server/router/api/v1/router.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authRouter from "./auth/router";
 import importRouter from "./import/router";
 import irRouter from "../../ir/router";
+import { FormatVersion } from "../../../../lib/constants/version";
 
 const router: Router = Router({ mergeParams: true });
 
@@ -9,4 +10,14 @@ router.use("/auth", authRouter);
 router.use("/import", importRouter);
 router.use("/ir", irRouter);
 
+router.use("/", (req, res) =>
+    res.status(200).json({
+        success: true,
+        description: "Request recieved successfully.",
+        body: {
+            serverTime: Date.now(),
+            version: FormatVersion(),
+        },
+    })
+);
 export default router;

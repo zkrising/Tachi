14a3122a1f35f148ec7a7398acecd9f7c5a2e8ed
zkldi <notzkldi@zkldi.com>
2021-06-25 14:27:37 +0100

    setindexes in router.test.ts

--- server/src/server/router/api/v1/search/router.test.ts
+++ server/src/server/router/api/v1/search/router.test.ts
@@ -1,9 +1,11 @@
 import t from "tap";
 import { CloseAllConnections } from "../../../../../test-utils/close-connections";
 import mockApi from "../../../../../test-utils/mock-api";
-import ResetDBState from "../../../../../test-utils/resets";
+import ResetDBState, { SetIndexesForDB } from "../../../../../test-utils/resets";
 import { LoadKTBlackIIDXData } from "../../../../../test-utils/test-data";
 import { AnySongDocument } from "tachi-common";
+
+t.before(SetIndexesForDB);
 
 t.test("GET /api/v1/search", (t) => {
 	t.beforeEach(ResetDBState);

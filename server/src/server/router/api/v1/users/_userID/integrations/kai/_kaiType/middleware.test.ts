c495eaf7f5faad2e3169ddce64aab3e654eccbd6
zkldi <ktchidev@gmail.com>
2021-08-12 19:20:45 +0100

    kai tests

---
+++ server/src/server/router/api/v1/users/_userID/integrations/kai/_kaiType/middleware.test.ts
@@ -0,0 +1,32 @@
+import t from "tap";
+import expMiddlewareMock from "express-request-mock";
+import { ValidateKaiType } from "./middleware";
+
+t.test("#ValidateKaiType", (t) => {
+	const k = async (k: string) => {
+		const { res } = await expMiddlewareMock(ValidateKaiType, { params: { kaiType: k } });
+
+		return res.statusCode;
+	};
+
+	t.test("Should allow flo, eag or min, case insensitively.", async (t) => {
+		t.equal(await k("flo"), 200);
+		t.equal(await k("eag"), 200);
+		t.equal(await k("min"), 200);
+		t.equal(await k("FLO"), 200);
+		t.equal(await k("EAG"), 200);
+		t.equal(await k("MIN"), 200);
+		t.equal(await k("FlO"), 200);
+		t.equal(await k("EaG"), 200);
+		t.equal(await k("MiN"), 200);
+		t.equal(await k("nonsense"), 400);
+		t.equal(await k("bad"), 400);
+		t.equal(await k(""), 400);
+		t.equal(await k("FLO2"), 400);
+		t.equal(await k("2FLO"), 400);
+
+		t.end();
+	});
+
+	t.end();
+});

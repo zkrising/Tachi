c5a15408ad68976157a0fcf1754dd3eaeecbefb5
Noah Heague <noahheague@gmail.com>
2021-08-15 18:42:28 +1000

    Allow bot to run in any channel

--- bot/src/utils/utils.ts
+++ bot/src/utils/utils.ts
@@ -2,6 +2,5 @@ import { Message } from "discord.js";
 import { allowedChannels } from "../config";
 
 export const shouldReply = (message: Message): boolean =>
-	allowedChannels.includes(message.channelId) &&
-  message.author.id !== "876320894295887944" &&
+	message.author.id !== "876320894295887944" &&
   !message.author.bot;

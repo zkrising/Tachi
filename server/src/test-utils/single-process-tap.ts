0c98298afe72f1324f33839ea678e1b3679e2d41
zkldi <ktchidev@gmail.com>
2021-08-26 03:16:24 +0100

    bind node_path properly

--- server/src/test-utils/single-process-tap.ts
+++ server/src/test-utils/single-process-tap.ts
@@ -1,6 +1,9 @@
 import glob from "glob";
+import path from "path";
 
-const files = glob.sync("**/*.test.ts");
+const files = glob.sync(path.join(__dirname, "../../", "**/*.test.ts"));
+
+process.env.NODE_PATH = ".";
 
 for (const file of files) {
 	require(file);

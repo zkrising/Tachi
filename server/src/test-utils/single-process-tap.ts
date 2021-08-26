97cb89793a7f78654b41a8dba9ccb3993bbe6366
zkldi <ktchidev@gmail.com>
2021-08-26 03:29:23 +0100

    Fix improper NODE_PATH dirname setting

--- server/src/test-utils/single-process-tap.ts
+++ server/src/test-utils/single-process-tap.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 const files = glob.sync(path.join(__dirname, "../../", "**/*.test.ts"));
 
-process.env.NODE_PATH = ".";
+process.env.NODE_PATH = path.join(__dirname, "../../");
 
 for (const file of files) {
 	require(file);

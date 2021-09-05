3d7a174ea9cdc30d0d2805275fa4d27e71786990
Noah Heague <noahheague@gmail.com>
2021-09-05 15:44:28 +1000

    Add extra env var DEV_SERVER_ID which defines where testing will take place

--- bot/scripts/createEnv.sh
+++ bot/scripts/createEnv.sh
@@ -9,8 +9,9 @@ if [[ "$ENV" != "PROD" ]]; then
   # Non-prod environment
   echo "ENV=DEV" >> "$DOT_ENV_PATH"
   echo "DISCORD_TOKEN=${NON_PROD_DISCORD_TOKEN:-"_"}" >> "$DOT_ENV_PATH"
+  echo "DEV_SERVER_ID=883949913887612938" >> "$DOT_ENV_PATH" # Where is testing taking place?
 else
   # Prod environment
- echo "ENV=PROD" >> "$DOT_ENV_PATH"
- echo "DISCORD_TOKEN=${PROD_DISCORD_TOKEN:-"_"}" >> "$DOT_ENV_PATH"
+  echo "ENV=PROD" >> "$DOT_ENV_PATH"
+  echo "DISCORD_TOKEN=${PROD_DISCORD_TOKEN:-"_"}" >> "$DOT_ENV_PATH"
 fi

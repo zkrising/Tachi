89b36bfc22b2f9d999fa66d305278035cda8e200
zkldi <ktchidev@gmail.com>
2021-09-07 12:47:19 +0100

    Add quickinsert and destroy scripts

---
+++ server/scripts/quick-insert/quick-insert.ts
@@ -0,0 +1,21 @@
+import deepmerge from "deepmerge";
+import { ICollection } from "monk";
+import { ChartDocument, IDStrings } from "tachi-common";
+import { Random20Hex } from "utils/misc";
+
+export async function QuickInsert<I extends IDStrings>(
+	base: ChartDocument<I>,
+	content: Partial<ChartDocument<I>>[],
+	coll: ICollection
+) {
+	const docs = [];
+
+	for (const c of content) {
+		const doc = deepmerge(base, c);
+
+		doc.chartID = Random20Hex();
+		docs.push(doc);
+	}
+
+	await coll.insert(docs);
+}

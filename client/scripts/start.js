a7f77bac43a5de34b58b7731d9316eaaf31d23fc
zkldi <ktchidev@gmail.com>
2021-09-10 06:54:50 +0100

    Disable open browser (its annoying)

--- client/scripts/start.js
+++ client/scripts/start.js
@@ -27,7 +27,6 @@ const {
   prepareProxy,
   prepareUrls,
 } = require('react-dev-utils/WebpackDevServerUtils');
-const openBrowser = require('react-dev-utils/openBrowser');
 const paths = require('../config/paths');
 const configFactory = require('../config/webpack.config');
 const createDevServerConfig = require('../config/webpackDevServer.config');
@@ -129,7 +128,6 @@ checkBrowsers(paths.appPath, isInteractive)
       }
 
       console.log(chalk.cyan('Starting the development server...\n'));
-      openBrowser(urls.localUrlForBrowser);
     });
 
     ['SIGINT', 'SIGTERM'].forEach(function(sig) {

70e0acb9b581876ccfde228f9c56edf70cde8f29
zkldi <ktchidev@gmail.com>
2021-09-16 19:05:24 +0100

    Export constants

--- common/src/index.ts
+++ common/src/index.ts
@@ -6,3 +6,4 @@ export * as ESDCore from "./lib/esd";
 export * as StaticConfig from "./config/static-config";
 export * from "./utils/util";
 export * from "./webhooks";
+export * from "./constants/game";

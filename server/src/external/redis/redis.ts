2600d715a76812692f09a78ebabb7247ffa54df0
zkldi <ktchidev@gmail.com>
2021-10-28 22:34:54 +0100

    Add proper redis logging

--- server/src/external/redis/redis.ts
+++ server/src/external/redis/redis.ts
@@ -1,6 +1,8 @@
+import { ONE_MINUTE } from "lib/constants/time";
 import CreateLogCtx from "lib/logger/logger";
 import { Environment } from "lib/setup/config";
 import redis from "redis";
+import { GetMilisecondsSince } from "utils/misc";
 
 const logger = CreateLogCtx(__filename);
 
@@ -9,19 +11,23 @@ logger.verbose("Instantiated Redis Store");
 export const RedisClient = redis.createClient({
 	url: `redis://${Environment.redisUrl}`,
 });
+const startConnect = process.hrtime.bigint();
+
 logger.verbose("Instantiated Redis Client");
 
 function EmitCritical() {
 	/* istanbul ignore next */
 	if (!RedisClient.connected) {
 		logger.crit(`Could not connect to redis in time. No more information is available.`);
+
+		process.exit(1);
 	}
 }
 
-const ref = setTimeout(EmitCritical, 30000);
+const ref = setTimeout(EmitCritical, ONE_MINUTE * 2);
 
 RedisClient.on("connect", () => {
-	logger.verbose(`Connected to Redis.`);
+	logger.info(`Connected to Redis. Took ${GetMilisecondsSince(startConnect)}ms`);
 
 	clearTimeout(ref);
 });

5cdbea3d3b2a9c3f4a227705f1cc8a51d4d9565d
zkldi <ktchidev@gmail.com>
2021-11-05 05:09:39 +0000

    Mount ksHookIR and change how fervidex permission mounting works.

---
+++ server/src/server/router/ir/kshook/router.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const router: Router = Router({ mergeParams: true });
+
+export default router;
--- server/src/server/router/ir/router.ts
+++ server/src/server/router/ir/router.ts
@@ -5,14 +5,16 @@ import directManualIR from "./direct-manual/router";
 import fervidexIR from "./fervidex/router";
 import uscIR from "./usc/router";
 import beatorajaIR from "./beatoraja/router";
+import ksHookIR from "./kshook/router";
 import { RequireBokutachi, RequireKamaitachi } from "../../middleware/type-require";
-import { SetRequestPermissions } from "../../middleware/auth";
+import { SetFervidexStyleRequestPermissions, SetRequestPermissions } from "../../middleware/auth";
 
 const router: Router = Router({ mergeParams: true });
 
 // Common IRs
 
 router.use("/direct-manual", SetRequestPermissions, directManualIR);
+router.use("/kshook", SetFervidexStyleRequestPermissions, ksHookIR);
 
 // Bokutachi IRs
 
@@ -24,9 +26,8 @@ router.use("/beatoraja", SetRequestPermissions, RequireBokutachi, beatorajaIR);
 
 // Kamaitachi IRs
 
-router.use(SetRequestPermissions);
-router.use("/barbatos", RequireKamaitachi, barbatosIR);
-router.use("/chunitachi", RequireKamaitachi, chunitachiIR);
-router.use("/fervidex", RequireKamaitachi, fervidexIR);
+router.use("/barbatos", SetRequestPermissions, RequireKamaitachi, barbatosIR);
+router.use("/chunitachi", SetRequestPermissions, RequireKamaitachi, chunitachiIR);
+router.use("/fervidex", SetFervidexStyleRequestPermissions, RequireKamaitachi, fervidexIR);
 
 export default router;

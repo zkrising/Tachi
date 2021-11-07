841556f2554778940fb4e3f0816daad05a0d8a9c
zkldi <ktchidev@gmail.com>
2021-11-07 04:25:33 +0000

    Define prettied versions.

--- common/src/config/static-config.ts
+++ common/src/config/static-config.ts
@@ -5,6 +5,7 @@ import {
 	ImportTypes,
 	Game,
 	IDStrings,
+	GPTSupportedVersions,
 } from "..";
 
 export const fileImportTypes: FileUploadImportTypes[] = [
@@ -71,3 +72,80 @@ export const allSupportedGames: Game[] = [
 	// "gitadora",
 	"usc",
 ];
+
+export type Versions = {
+	[I in IDStrings]: {
+		[K in GPTSupportedVersions[I]]: string;
+	};
+};
+
+const prettyIIDXVersions: Versions["iidx:SP"] = {
+	20: "tricoro",
+	21: "SPADA",
+	22: "PENDUAL",
+	23: "copula",
+	24: "SINOBUZ",
+	25: "CANNON BALLERS",
+	26: "ROOTAGE",
+	27: "HEROIC VERSE",
+	28: "BISTROVER",
+	29: "CastHour",
+	"10-cs": "10th Style CS",
+	"11-cs": "IIDX RED CS",
+	"12-cs": "HAPPY SKY CS",
+	"13-cs": "DISTORTED CS",
+	"14-cs": "GOLD CS",
+	"15-cs": "DJ TROOPERS CS",
+	"16-cs": "EMPRESS CS",
+	"26-omni": "ROOTAGE Omnimix",
+	"3-cs": "3rd Style CS",
+	"4-cs": "4th Style CS",
+	"5-cs": "5th Style CS",
+	"6-cs": "6th Style CS",
+	"7-cs": "7th Style CS",
+	"8-cs": "8th Style CS",
+	"9-cs": "9th Style CS",
+	"27-omni": "HEROIC VERSE Omnimix",
+	"28-omni": "BISTROVER Omnimix",
+	"27-2dxtra": "HEROIC VERSE 2dxtra",
+	bmus: "BEATMANIA US",
+	inf: "INFINITAS",
+};
+
+const prettyDDRVersions: Versions["ddr:SP"] = {
+	a20: "A20",
+};
+
+const prettyGitadoraVersions: Versions["gitadora:Dora"] = {
+	nextage: "NEX+AGE",
+};
+
+export const PrettyVersions: Versions = {
+	"iidx:SP": prettyIIDXVersions,
+	"iidx:DP": prettyIIDXVersions,
+	"bms:7K": {},
+	"bms:14K": {},
+	"chunithm:Single": {
+		paradise: "Paradise",
+	},
+	"ddr:SP": prettyDDRVersions,
+	"ddr:DP": prettyDDRVersions,
+	"gitadora:Gita": prettyGitadoraVersions,
+	"gitadora:Dora": prettyGitadoraVersions,
+	"maimai:Single": {
+		finale: "FiNALE",
+	},
+	"museca:Single": {
+		1.5: "1 + 1/2",
+		"1.5-b": "1 + 1/2 Rev. B",
+	},
+	"sdvx:Single": {
+		booth: "BOOTH",
+		inf: "Infinite Infection",
+		gw: "GRAVITY WARS",
+		heaven: "HEAVENLY HAVEN",
+		vivid: "VIVID WAVE",
+		konaste: "Konaste",
+	},
+	"usc:Single": {},
+};

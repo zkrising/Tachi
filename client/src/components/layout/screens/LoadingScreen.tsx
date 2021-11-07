075f296f417f876fd8864859ac06b1d364e959f1
zkldi <ktchidev@gmail.com>
2021-11-07 06:17:24 +0000

    Change down message

--- client/src/components/layout/screens/LoadingScreen.tsx
+++ client/src/components/layout/screens/LoadingScreen.tsx
@@ -43,7 +43,7 @@ export function LoadingScreen({ children }: JustChildren) {
 			.catch(err => {
 				console.error(err);
 				setBroke(
-					`A network error has occured while loading ${TachiConfig.name}. If you think this is on your end, try reloading the page. If you think it isn't, report this.`
+					`${TachiConfig.name} is currently down, sadly. Check back in a while, or we might be doing a quick server restart. Sorry!`
 				);
 				clearTimeout(timeout);
 			});

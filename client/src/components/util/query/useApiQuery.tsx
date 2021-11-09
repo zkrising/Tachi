ead7f6826a6c02620b77ac6ce26c4c05fa7a58d6
zkldi <20380519+zkldi@users.noreply.github.com>
2021-11-09 15:45:43 +0000

    Make post requests never repeat

--- client/src/components/util/query/useApiQuery.tsx
+++ client/src/components/util/query/useApiQuery.tsx
@@ -3,23 +3,29 @@ import { SuccessfulAPIResponse } from "tachi-common";
 import { APIFetchV1, UnsuccessfulAPIFetchResponse } from "util/api";
 
 export default function useApiQuery<T>(url: string | string[], options?: RequestInit) {
-	return useQuery<T, UnsuccessfulAPIFetchResponse>(url, async () => {
-		if (Array.isArray(url)) {
-			const results = await Promise.all(url.map(u => APIFetchV1(u, options)));
+	return useQuery<T, UnsuccessfulAPIFetchResponse>(
+		url,
+		async () => {
+			if (Array.isArray(url)) {
+				const results = await Promise.all(url.map(u => APIFetchV1(u, options)));
 
-			if (!results.every(r => r.success)) {
-				throw results;
+				if (!results.every(r => r.success)) {
+					throw results;
+				}
+
+				return (results.map(e => (e as SuccessfulAPIResponse).body) as unknown) as T;
 			}
 
-			return (results.map(e => (e as SuccessfulAPIResponse).body) as unknown) as T;
+			const res = await APIFetchV1<T>(url, options);
+
+			if (!res.success) {
+				throw res;
+			}
+
+			return res.body;
+		},
+		{
+			retry: (options?.method ?? "GET") === "GET",
 		}
-
-		const res = await APIFetchV1<T>(url, options);
-
-		if (!res.success) {
-			throw res;
-		}
-
-		return res.body;
-	});
+	);
 }

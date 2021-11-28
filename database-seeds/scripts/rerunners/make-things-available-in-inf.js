b62941d35367bb6c3b523a4c7408afe2328a00b0
zkldi <20380519+zkldi@users.noreply.github.com>
2021-11-28 08:39:22 +0000

    Script shouldn't mark beginners or leggs as infinitas ready.

--- database-seeds/scripts/rerunners/make-things-available-in-inf.js
+++ database-seeds/scripts/rerunners/make-things-available-in-inf.js
@@ -3,7 +3,7 @@ const { MutateCollection } = require("../util");
 module.exports = (idSet) => {
 	return MutateCollection("charts-iidx.json", (charts) => {
 		for (const chart of charts) {
-			if (idSet.includes(chart.data.inGameID) && !chart.versions.includes("inf") && chart.data["2dxtraSet"] === null) {
+			if (idSet.includes(chart.data.inGameID) && !chart.versions.includes("inf") && chart.data["2dxtraSet"] === null && chart.difficulty !== "LEGGENDARIA" && chart.difficulty !== "BEGINNER") {
 				chart.versions.push("inf");
 			}
 		}

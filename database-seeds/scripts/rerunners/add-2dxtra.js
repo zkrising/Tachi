79a3bfa58d89e43b8e00c65d307921590b69f73b
zkldi <20380519+zkldi@users.noreply.github.com>
2021-12-07 12:41:46 +0000

    create add-2dxtra script

---
+++ database-seeds/scripts/rerunners/add-2dxtra.js
@@ -0,0 +1,128 @@
+const { Command } = require("commander");
+const { XMLParser } = require("fast-xml-parser");
+const fs = require("fs");
+const { MutateCollection, CreateChartID } = require("../util");
+
+const program = new Command();
+program
+	.option("-f, --file <XML File>")
+	.option("-n, --name <Set Name>")
+	.option("-v, --version <Version>");
+
+program.parse(process.argv);
+const options = program.opts();
+
+if (!options.file || !options.name || !options.version) {
+	throw new Error("Missing options --file, --name or --version.");
+}
+
+const parser = new XMLParser({ ignoreAttributes: false });
+
+const data = parser.parse(fs.readFileSync(options.file));
+
+const parsedData = [];
+
+function SplitFervidexChartRef(ferDif) {
+	let playtype;
+
+	if (ferDif.startsWith("sp")) {
+		playtype = "SP";
+	} else {
+		playtype = "DP";
+	}
+
+	let difficulty;
+
+	switch (ferDif[ferDif.length - 1]) {
+		case "b":
+			difficulty = "BEGINNER";
+			break;
+		case "n":
+			difficulty = "NORMAL";
+			break;
+		case "h":
+			difficulty = "HYPER";
+			break;
+		case "a":
+			difficulty = "ANOTHER";
+			break;
+		case "l":
+			difficulty = "LEGGENDARIA";
+			break;
+		default:
+			throw new InternalFailure(`Invalid fervidex difficulty of ${ferDif}`);
+	}
+
+	return { playtype, difficulty };
+}
+
+for (const info of data.entries.music) {
+	const id = Number(info["@_id"]);
+
+	if (!Array.isArray(info.chart)) {
+		info.chart = [info.chart];
+	}
+
+	for (const chart of info.chart) {
+		const hash = chart["@_id"];
+		const diff = chart["@_type"];
+		const notes = Number(chart["@_notes"]);
+
+		const { playtype, difficulty } = SplitFervidexChartRef(diff);
+
+		parsedData.push({
+			id,
+			hash,
+			notes,
+			playtype,
+			difficulty
+		})
+	}
+}
+
+MutateCollection("charts-iidx.json", (charts) => {
+	for (const data of parsedData) {
+		let match = false;
+		let existingReference = null;
+		for (const chart of charts) {
+			if (chart.data.hashSHA256 === data.hash) {
+				// chart.versions.push(options.version);
+				match = true;
+				break;
+			} else if (chart.data.inGameID === data.id) {
+				existingReference = chart;
+			}
+		}
+
+		if (!match) {
+			if (!existingReference) {
+				console.log(`Couldn't resolve this: `, data);
+				continue;
+			}
+
+			// charts.push({
+			// 	"chartID": CreateChartID(),
+			// 	"data": {
+			// 		"2dxtraSet": options.set,
+			// 		"arcChartID": null,
+			// 		"hashSHA256": data.hash,
+			// 		"inGameID": data.id,
+			// 		"notecount": data.notes
+			// 	},
+			// 	"difficulty": `${options.set} ${data.difficulty}`,
+			// 	"isPrimary": true,
+			// 	"level": "?",
+			// 	"levelNum": 0,
+			// 	playtype: data.playtype,
+			// 	"rgcID": null,
+			// 	"songID": existingReference.songID,
+			// 	"tierlistInfo": {},
+			// 	"versions": [
+			// 		options.version
+			// 	]
+			// });
+		}
+	}
+
+	return charts;
+});

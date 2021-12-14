37f26ea6d1ae6ea67e6f8d09e32042e1c6d0388f
zkldi <20380519+zkldi@users.noreply.github.com>
2021-12-14 19:42:24 +0000

    Bump version

--- client/src/util/constants/version.ts
+++ client/src/util/constants/version.ts
@@ -1,6 +1,6 @@
 const MAJOR = 2;
 const MINOR = 0;
-const PATCH = 3;
+const PATCH = 4;
 
 // As is with all front-facing zkldi projects, the version names for tachi-client
 // are from an album I like. In this case, the album is The Cure - Disintegration.

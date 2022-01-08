96aa862641985c20c473ae19d16783b0f6407e5f
zkldi <20380519+zkldi@users.noreply.github.com>
2022-01-08 18:17:03 +0000

    Enforce that the USC IR still orphans scores, instead of rejecting unknown charts.

--- server/src/lib/constants/version.ts
+++ server/src/lib/constants/version.ts
@@ -4,7 +4,7 @@
 
 const MAJOR = 2;
 const MINOR = 0;
-const PATCH = 28;
+const PATCH = 29;
 
 // As is with all front-facing zkldi projects, the version names for tachi-server
 // are from an album I like. In this case, the album is Portishead - Dummy.

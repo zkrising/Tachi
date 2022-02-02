63a9b74a4d9d4cd8746816d3c5a22f751be2c5a7
zkldi <20380519+zkldi@users.noreply.github.com>
2022-02-02 02:52:49 +0000

    Support popn background cells

--- client/src/components/tables/cells/TitleCell.tsx
+++ client/src/components/tables/cells/TitleCell.tsx
@@ -2,6 +2,7 @@ import GentleLink from "components/util/GentleLink";
 import Muted from "components/util/Muted";
 import React from "react";
 import { ChartDocument, Game, SongDocument } from "tachi-common";
+import { ToCDNURL } from "util/api";
 import { CreateChartLink } from "util/data";
 
 export default function TitleCell({
@@ -17,8 +18,24 @@ export default function TitleCell({
 	noArtist?: boolean;
 	comment?: string | null;
 }) {
+	const backgroundImage =
+		game !== "popn"
+			? undefined
+			: `linear-gradient(to left, rgba(19, 19, 19, 0.1), rgba(19, 19, 19, 1)), url(${ToCDNURL(
+					`/misc/popn/banners/${(chart as any).data.inGameID}.png`
+			  )})`;
+
 	return (
-		<td style={{ textAlign: "left", minWidth: "140px", maxWidth: "300px" }}>
+		<td
+			style={{
+				textAlign: "left",
+				minWidth: "140px",
+				maxWidth: "300px",
+				backgroundRepeat: "no-repeat",
+				backgroundSize: "cover",
+				backgroundImage,
+			}}
+		>
 			{game === "popn" && (
 				<>
 					{(song as SongDocument<"popn">).data.genre === song.title ||

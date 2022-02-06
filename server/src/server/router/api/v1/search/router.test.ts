7a1d7575e9b5e8379813bc389848056bad8a0f8d
zkldi <20380519+zkldi@users.noreply.github.com>
2022-02-06 21:45:59 +0000

    Support searching on BMS/PMS chart hashes Fixes #630

--- server/src/server/router/api/v1/search/router.test.ts
+++ server/src/server/router/api/v1/search/router.test.ts
@@ -2,7 +2,7 @@ import t from "tap";
 
 import mockApi from "test-utils/mock-api";
 import ResetDBState from "test-utils/resets";
-import { LoadTachiIIDXData } from "test-utils/test-data";
+import { BMSGazerChart, LoadTachiIIDXData } from "test-utils/test-data";
 import { SongDocument } from "tachi-common";
 
 t.test("GET /api/v1/search", (t) => {
@@ -35,6 +35,70 @@ t.test("GET /api/v1/search", (t) => {
 		const res = await mockApi.get("/api/v1/search");
 
 		t.equal(res.statusCode, 400);
+
+		t.end();
+	});
+
+	t.end();
+});
+
+t.test("GET /api/v1/search/chart-hash", (t) => {
+	const BMS_MD5 = "38616b85332037cc12924f2ae2840262";
+	const BMS_SHA2 = "195fe1be5c3e74fccd04dc426e05f8a9cfa8a1059c339d0a23e99f63661f0b7d";
+	const PMS_MD5 = "d1253dd56bb2087d0b0d474f0d562aae";
+	const PMS_SHA2 = "a10193f7ae05ce839292dc716f182fda0b1cc6ac5382c2056f37e22ffba87b7d";
+	const USC_SHA1 = "USC_CHART_HASH"; // this is apparantly what our mock data uses.
+
+	t.test("Should return a BMS chart if the MD5 matches.", async (t) => {
+		const res = await mockApi.get(`/api/v1/search/chart-hash?search=${BMS_MD5}`);
+
+		t.equal(res.body.body.charts[0].data.hashMD5, BMS_MD5);
+		t.equal(res.body.body.songs[0].game, "bms");
+
+		t.end();
+	});
+
+	t.test("Should return a BMS chart if the SHA2 matches.", async (t) => {
+		const res = await mockApi.get(`/api/v1/search/chart-hash?search=${BMS_SHA2}`);
+
+		t.equal(res.body.body.charts[0].data.hashSHA256, BMS_SHA2);
+		t.equal(res.body.body.songs[0].game, "bms");
+
+		t.end();
+	});
+
+	t.test("Should return a PMS chart if the MD5 matches.", async (t) => {
+		const res = await mockApi.get(`/api/v1/search/chart-hash?search=${PMS_MD5}`);
+
+		t.equal(res.body.body.charts[0].data.hashMD5, PMS_MD5);
+		t.equal(res.body.body.songs[0].game, "pms");
+
+		t.end();
+	});
+
+	t.test("Should return a PMS chart if the SHA2 matches.", async (t) => {
+		const res = await mockApi.get(`/api/v1/search/chart-hash?search=${PMS_SHA2}`);
+
+		t.equal(res.body.body.charts[0].data.hashSHA256, PMS_SHA2);
+		t.equal(res.body.body.songs[0].game, "pms");
+
+		t.end();
+	});
+
+	t.test("Should return a USC chart if the SHA1 matches.", async (t) => {
+		const res = await mockApi.get(`/api/v1/search/chart-hash?search=${USC_SHA1}`);
+
+		t.equal(res.body.body.charts[0].data.hashSHA1, USC_SHA1);
+		t.equal(res.body.body.songs[0].game, "usc");
+
+		t.end();
+	});
+
+	t.test("Should return nothing if the hash matches nothing.", async (t) => {
+		const res = await mockApi.get(`/api/v1/search/chart-hash?search=CHART_THAT_DOESNT_EXIST`);
+
+		t.equal(res.body.body.charts.length, 0);
+		t.equal(res.body.body.songs.length, 0);
 
 		t.end();
 	});
--- server/src/server/router/api/v1/search/router.ts
+++ server/src/server/router/api/v1/search/router.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
-import { SearchAllGamesSongs, SearchUsersRegExp } from "lib/search/search";
+import db from "external/mongo/db";
+import { SearchAllGamesSongs, SearchForChartHash, SearchUsersRegExp } from "lib/search/search";
+import { RequireBokutachi } from "server/middleware/type-require";
 import { IsString } from "utils/misc";
 
 const router: Router = Router({ mergeParams: true });
@@ -30,6 +32,33 @@ router.get("/", async (req, res) => {
 		body: {
 			users,
 			songs,
+		},
+	});
+});
+
+/**
+ * Search checksums for charts, instead of matching on song title.
+ *
+ * @param search - The hash to search on
+ *
+ * @note This matches MD5 and SHA256 for BMS/PMS, and SHA1 for USC.
+ */
+router.get("/chart-hash", RequireBokutachi, async (req, res) => {
+	if (!IsString(req.query.search)) {
+		return res.status(400).json({
+			success: false,
+			description: "No search parameter given.",
+		});
+	}
+
+	const { songs, charts } = await SearchForChartHash(req.query.search);
+
+	return res.status(200).json({
+		success: true,
+		description: `Searched BMS, PMS and USC for ${req.query.search}().`,
+		body: {
+			songs,
+			charts,
 		},
 	});
 });

abb4501c6da66b9939ee73c11b4f4242718fb336
zkldi <20380519+zkldi@users.noreply.github.com>
2022-03-22 18:58:53 +0000

    Set version to v2.1.9

--- server/src/lib/constants/version.ts
+++ server/src/lib/constants/version.ts
@@ -3,8 +3,8 @@
 // essentially share codebases.
 
 const MAJOR = 2;
-const MINOR = 2;
-const PATCH = 0;
+const MINOR = 1;
+const PATCH = 9;
 
 // As is with all front-facing zkldi projects, the version names for tachi-server
 // are from an album I like. In this case, the album is Portishead - Dummy.

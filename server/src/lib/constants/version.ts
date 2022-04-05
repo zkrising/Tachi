2c4313976e129afe38a79840d9afbdaece2c29bf
zkldi <20380519+zkldi@users.noreply.github.com>
2022-04-05 01:48:42 +0100

    Whoops, forgot to bump version.

--- server/src/lib/constants/version.ts
+++ server/src/lib/constants/version.ts
@@ -4,7 +4,7 @@
 
 const MAJOR = 2;
 const MINOR = 1;
-const PATCH = 14;
+const PATCH = 15;
 
 // As is with all front-facing zkldi projects, the version names for tachi-server
 // are from an album I like. In this case, the album is Portishead - Dummy.

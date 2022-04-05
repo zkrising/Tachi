98c3fb97177695d4139a5f27e1a3bf2d2a6da3b7
zkldi <20380519+zkldi@users.noreply.github.com>
2022-04-05 19:34:46 +0100

    Enforce minimum scores in classUpdate.

--- bot/src/webhookHandlers/classUpdate.ts
+++ bot/src/webhookHandlers/classUpdate.ts
@@ -1,7 +1,8 @@
-import { FormatGame, integer, WebhookEventClassUpdateV1 } from "tachi-common";
+import { FormatGame, Game, integer, Playtype, WebhookEventClassUpdateV1 } from "tachi-common";
+import { AllClassSets } from "tachi-common/js/game-classes";
 import { BotConfig } from "../config";
 import { client } from "../main";
-import { GetUserInfo } from "../utils/apiRequests";
+import { GetUGPTStats, GetUserInfo } from "../utils/apiRequests";
 import { CreateEmbed } from "../utils/embeds";
 import { PrependTachiUrl } from "../utils/fetchTachi";
 import logger from "../utils/logger";
@@ -23,6 +24,22 @@ export async function HandleClassUpdateV1(
 
 	const userDoc = await GetUserInfo(event.userID);
 
+	// We don't want to render classes if the user is blitzing through them
+	// because they haven't played enough charts to "average out".
+	// For example, a new player playing their first 50 charts will blitz through
+	// atleast 10 of the volforce ranks, which will just result in channel spam.
+	const minimumNecessaryScores = GetMinimumScores(game, playtype, event.set);
+
+	if (minimumNecessaryScores) {
+		const { totalScores } = await GetUGPTStats(userDoc.id, game, playtype);
+
+		// Do not render if the user hasn't hit the score cap.
+		if (totalScores < minimumNecessaryScores) {
+			logger.info(`Not rendering class update ${event.set}: ${event.old} -> ${event.new}.`);
+			return 204;
+		}
+	}
+
 	const newClass = FormatClass(game, playtype, event.set, event.new);
 
 	const embed = CreateEmbed()
@@ -41,4 +58,24 @@ export async function HandleClassUpdateV1(
 	await channel.send({ embeds: [embed] });
 
 	return 200;
+}
+
+function GetMinimumScores(game: Game, playtype: Playtype, classSet: AllClassSets): integer | null {
+	if (game === "chunithm") {
+		return 20;
+	} else if (game === "sdvx" && classSet === "vfClass") {
+		return 50;
+	} else if (game === "gitadora") {
+		return 50;
+	} else if (game === "jubeat") {
+		return 60;
+	} else if (game === "wacca") {
+		return 50;
+	} else if (game === "bms") {
+		return 20;
+	} else if (game === "iidx") {
+		return 20;
+	}
+
+	return null;
 }

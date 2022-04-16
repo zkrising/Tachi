cd57962a8f231411803b6ef0fe64f3d3679192fc
zkldi <zkldi.dev@gmail.com>
2022-04-16 20:02:28 +0000

    test: add tests for gpt/milestones/popular

--- server/src/server/router/api/v1/games/_game/_playtype/targets/milestones/router.test.ts
+++ server/src/server/router/api/v1/games/_game/_playtype/targets/milestones/router.test.ts
@@ -1,9 +1,13 @@
-import { MilestoneDocument } from "tachi-common";
+import { MilestoneDocument, MilestoneSubscriptionDocument } from "tachi-common";
 import t from "tap";
 import mockApi from "test-utils/mock-api";
 import ResetDBState from "test-utils/resets";
 import dm from "deepmerge";
-import { TestingIIDXSPMilestone } from "test-utils/test-data";
+import {
+	FakeOtherUser,
+	TestingIIDXSPMilestone,
+	TestingIIDXSPMilestoneSub,
+} from "test-utils/test-data";
 import db from "external/mongo/db";
 
 t.test("GET /api/v1/games/:game/:playtype/targets/milestones", (t) => {
@@ -33,6 +37,45 @@ t.test("GET /api/v1/games/:game/:playtype/targets/milestones", (t) => {
 		t.hasStrict(
 			(res.body.body as MilestoneDocument[]).sort((a, b) => a.name.localeCompare(b.name)),
 			[{ milestoneID: "similar_name" }, { milestoneID: "name" }]
+		);
+
+		t.end();
+	});
+
+	t.end();
+});
+
+// this is my lazy sample data for these tests.
+const LoadLazySampleData = async () => {
+	await db.users.insert(FakeOtherUser);
+	await db.milestones.insert([
+		TestingIIDXSPMilestone,
+		dm(TestingIIDXSPMilestone, { milestoneID: "other_milestone" }) as MilestoneDocument,
+	]);
+	await db["milestone-subs"].insert([
+		TestingIIDXSPMilestoneSub,
+		dm(TestingIIDXSPMilestoneSub, { milestoneID: "other_milestone" }),
+		dm(TestingIIDXSPMilestoneSub, {
+			userID: 2,
+		}),
+	] as MilestoneSubscriptionDocument[]);
+};
+
+t.test("GET /api/v1/games/:game/:playtype/targets/milestones/popular", (t) => {
+	t.beforeEach(ResetDBState);
+	t.beforeEach(LoadLazySampleData);
+
+	t.test("Should return the most subscribed milestones for this GPT.", async (t) => {
+		const res = await mockApi.get("/api/v1/games/iidx/SP/targets/milestones/popular");
+
+		t.equal(res.statusCode, 200);
+
+		t.hasStrict(
+			(res.body.body as MilestoneDocument[]).sort((a, b) => a.name.localeCompare(b.name)),
+			[
+				{ milestoneID: TestingIIDXSPMilestone.milestoneID, __subscriptions: 2 },
+				{ milestoneID: "other_milestone", __subscriptions: 1 },
+			]
 		);
 
 		t.end();

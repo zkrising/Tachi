ecc5f9c4f2602a7d1458afdb1135bde43466f692
zkldi <zkldi.dev@gmail.com>
2022-04-16 20:58:02 +0000

    scratch: BROKEN TESTS. PUSHING SO I CAN CONTINUE WORK AT HOME

--- server/src/server/router/api/v1/games/_game/_playtype/targets/milestones/router.test.ts
+++ server/src/server/router/api/v1/games/_game/_playtype/targets/milestones/router.test.ts
@@ -54,6 +54,7 @@ t.test("GET /api/v1/games/:game/:playtype/targets/milestones", (t) => {
 // this is my lazy sample data for these tests.
 const LoadLazySampleData = async () => {
 	await db.users.insert(FakeOtherUser);
+	await db.goals.insert(IIDXSPMilestoneGoals);
 	await db.milestones.insert([
 		TestingIIDXSPMilestone,
 		dm(TestingIIDXSPMilestone, { milestoneID: "other_milestone" }) as MilestoneDocument,
@@ -94,7 +95,6 @@ t.test("GET /api/v1/games/:game/:playtype/targets/milestones/:milestoneID", (t) 
 	t.beforeEach(ResetDBState);
 	t.beforeEach(async () => {
 		await Promise.all([
-			db.goals.insert(IIDXSPMilestoneGoals),
 			db["goal-subs"].insert(IIDXSPMilestoneGoalSubs),
 			db["milestone-sets"].insert({
 				setID: "set_id",
@@ -102,6 +102,7 @@ t.test("GET /api/v1/games/:game/:playtype/targets/milestones/:milestoneID", (t) 
 			} as MilestoneSetDocument),
 		]);
 	});
+
 	t.beforeEach(LoadLazySampleData);
 
 	t.test("Should return the milestone and its goals.", async (t) => {
@@ -139,6 +140,172 @@ t.test("GET /api/v1/games/:game/:playtype/targets/milestones/:milestoneID", (t) 
 		);
 
 		t.equal(res.statusCode, 404);
+
+		t.end();
+	});
+
+	t.end();
+});
+
+t.test("GET /api/v1/games/:game/:playtype/targets/milestones/:milestoneID/evaluate-for", (t) => {
+	t.beforeEach(ResetDBState);
+	t.beforeEach(LoadLazySampleData);
+
+	t.test("Should evaluate a milestone for a user.", async (t) => {
+		// remove all milestone subs -- the user isn't properly
+		// subscribed to this milestone due to test mocking.
+		// this will force a live calc.
+		await db["milestone-subs"].remove({});
+
+		const res = await mockApi.get(
+			`/api/v1/games/iidx/SP/targets/milestones/${TestingIIDXSPMilestone.milestoneID}/evaluate-for?userID=1`
+		);
+
+		t.equal(res.statusCode, 200);
+
+		t.strictSame(res.body.body, {
+			goals: [
+				{
+					charts: {
+						type: "single",
+						data: "c2311194e3897ddb5745b1760d2c0141f933e683",
+					},
+					game: "iidx",
+					goalID: "eg_goal_1",
+					playtype: "SP",
+					timeAdded: 0,
+					name: "HC 5.1.1. SPA",
+					criteria: {
+						mode: "single",
+						value: 5,
+						key: "scoreData.lampIndex",
+					},
+				},
+				{
+					charts: {
+						type: "single",
+						data: "c2311194e3897ddb5745b1760d2c0141f933e683",
+					},
+					game: "iidx",
+					goalID: "eg_goal_2",
+					playtype: "SP",
+					timeAdded: 0,
+					name: "HC 5.1.1. SPA",
+					criteria: {
+						mode: "single",
+						value: 2,
+						key: "scoreData.lampIndex",
+					},
+				},
+				{
+					charts: {
+						type: "single",
+						data: "c2311194e3897ddb5745b1760d2c0141f933e683",
+					},
+					game: "iidx",
+					goalID: "eg_goal_3",
+					playtype: "SP",
+					timeAdded: 0,
+					name: "HC 5.1.1. SPA",
+					criteria: {
+						mode: "single",
+						value: 300,
+						key: "scoreData.lampIndex",
+					},
+				},
+				{
+					charts: {
+						type: "single",
+						data: "c2311194e3897ddb5745b1760d2c0141f933e683",
+					},
+					game: "iidx",
+					goalID: "eg_goal_4",
+					playtype: "SP",
+					timeAdded: 0,
+					name: "HC 5.1.1. SPA",
+					criteria: {
+						mode: "single",
+						value: 1100,
+						key: "scoreData.lampIndex",
+					},
+				},
+			],
+			goalResults: [
+				{
+					achieved: false,
+					progress: null,
+					outOf: 5,
+					progressHuman: "NO DATA",
+					outOfHuman: "HARD CLEAR",
+					goalID: "eg_goal_1",
+				},
+				{
+					achieved: false,
+					progress: null,
+					outOf: 2,
+					progressHuman: "NO DATA",
+					outOfHuman: "ASSIST CLEAR",
+					goalID: "eg_goal_2",
+				},
+				{
+					achieved: false,
+					progress: null,
+					outOf: 300,
+					progressHuman: "NO DATA",
+					goalID: "eg_goal_3",
+				},
+				{
+					achieved: false,
+					progress: null,
+					outOf: 1100,
+					progressHuman: "NO DATA",
+					goalID: "eg_goal_4",
+				},
+			],
+			achieved: false,
+			progress: 0,
+			outOf: 4,
+		});
+
+		t.end();
+	});
+
+	t.test("Should return 404 if the requested milestone doesn't exist.", async (t) => {
+		const res = await mockApi.get(
+			"/api/v1/games/iidx/SP/targets/milestones/fake_milestone/evaluate-for?userID=1"
+		);
+
+		t.equal(res.statusCode, 404);
+
+		t.end();
+	});
+
+	t.test("Should return 404 if the milestone exists but for a different GPT.", async (t) => {
+		const res = await mockApi.get(
+			`/api/v1/games/iidx/DP/targets/milestones/${TestingIIDXSPMilestone.milestoneID}/evaluate-for?userID=1`
+		);
+
+		t.equal(res.statusCode, 404);
+
+		t.end();
+	});
+
+	t.test("Should return 404 if the requested user doesn't exist.", async (t) => {
+		const res = await mockApi.get(
+			`/api/v1/games/iidx/SP/targets/milestones/${TestingIIDXSPMilestone.milestoneID}/evaluate-for?userID=3`
+		);
+
+		t.equal(res.statusCode, 404);
+
+		t.end();
+	});
+
+	t.test("Should return 400 if the requested user hasn't played this gpt.", async (t) => {
+		const res = await mockApi.get(
+			`/api/v1/games/iidx/SP/targets/milestones/${TestingIIDXSPMilestone.milestoneID}/evaluate-for?userID=2`
+		);
+
+		t.equal(res.statusCode, 400);
 
 		t.end();
 	});

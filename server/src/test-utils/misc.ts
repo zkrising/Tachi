3205a82a05575e972102b5af1650499bf15a37a7
zkldi <20380519+zkldi@users.noreply.github.com>
2022-04-21 06:01:34 +0100

    refactor(test-utils): add cleaner convenient test utils

--- server/src/test-utils/misc.ts
+++ server/src/test-utils/misc.ts
@@ -1,3 +1,7 @@
+import deepmerge from "deepmerge";
+import { Game, integer, Playtype } from "tachi-common";
+import { FakeGameSettings, FakeOtherUser } from "./test-data";
+
 /**
  * Async Generator To Array
  */
@@ -8,4 +12,33 @@ export async function agta(ag: AsyncIterable<unknown> | Iterable<unknown>) {
 	}
 
 	return a;
+}
+
+/**
+ * Deep-modify an object. This is a wrapper around deepmerge that returns proper types.
+ */
+export function dmf<T extends object>(base: T, modifant: Partial<T>): T {
+	return deepmerge(base, modifant) as T;
+}
+
+/**
+ * Make a fake user for testing. This automatically sets the username to something
+ * unique (to avoid index collisions)
+ *
+ * @param userID - The userID this fake user should have.
+ */
+export function mkFakeUser(userID: integer) {
+	return dmf(FakeOtherUser, {
+		id: userID,
+		username: `user${userID}`,
+		usernameLowercase: `user${userID}`,
+	});
+}
+
+export function mkFakeGameSettings(userID: integer, game: Game, playtype: Playtype) {
+	return dmf(FakeGameSettings, {
+		userID,
+		game,
+		playtype,
+	});
 }

c5cf11ab4622e2e5e20cdbb6e3edb3aeac7ce34c
zkldi <20380519+zkldi@users.noreply.github.com>
2022-04-22 18:17:27 +0100

    fix: actually export permission constants

--- common/src/index.ts
+++ common/src/index.ts
@@ -7,4 +7,5 @@ export * as StaticConfig from "./config/static-config";
 export * from "./utils/util";
 export * from "./webhooks";
 export * from "./constants/game";
+export * from "./constants/permissions";
 export * as Schemas from "./lib/schemas";

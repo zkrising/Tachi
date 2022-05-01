edc844e7d3f42b6065338c2b56b697492202267e
Christopher Cooper <christopher.c.cooper@gmail.com>
2022-04-30 20:01:40 -0700

    allow run-tests.sh to be called from other directories
    Also, just use tee non-appending mode instead of deleting the file.

--- database-seeds/scripts/run-tests.sh
+++ database-seeds/scripts/run-tests.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 # Output stderr and stdout to the terminal, but only save stderr to a file
 # Isn't bash wonderful?
 
-# I can't figure out how to make it write to failed-tests.log instead of appending
-# so, whatever.
-# look. I don't like bash much.
-# I'd rather spend 0% of my time here.
-rm failed-tests.log -f
-
-ts-node test/collections.test.ts 2> >(tee -a failed-tests.log >&2)
+ts-node test/collections.test.ts 2> >(tee failed-tests.log >&2)
 ts-node test/id.test.ts 2> >(tee -a failed-tests.log >&2)

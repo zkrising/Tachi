ee73b5f251016b6ede43b3f6b7ad09e3eb0907dd
zkldi <20380519+zkldi@users.noreply.github.com>
2022-06-02 01:45:04 +0100

    fix: add a warning about .xyz isms

--- client/src/components/layout/misc/EmailVerify.tsx
+++ client/src/components/layout/misc/EmailVerify.tsx
@@ -41,6 +41,12 @@ export default function EmailVerify({
 			An email has been sent to the address you signed up with.
 			<br />
 			It might've ended up in your spam, so check there too!
+			<br />
+			<b>
+				Our domain ends with <code>.xyz</code>! As such, most email services will throw our
+				email in spam, or hold it in limbo for a couple minutes. Please be patient. Sending
+				email from an <code>.xyz</code> domain SUCKS!
+			</b>
 			<Divider />
 			<Button
 				onClick={async () => {

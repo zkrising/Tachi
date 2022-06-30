ee25b34648e6f26057fb288b3abdf83b39e8fd91
zkldi <20380519+zkldi@users.noreply.github.com>
2022-06-30 14:24:01 +0100

    fix: check user exists before we try and set it in the map

--- client/src/app/pages/dashboard/games/_game/_playtype/GPTChartPage.tsx
+++ client/src/app/pages/dashboard/games/_game/_playtype/GPTChartPage.tsx
@@ -147,9 +147,11 @@ function InternalGPTChartPage({
 			userMap.set(user.id, user);
 		}
 	}
-	
-	// Add current user, since there's no guarantee they are returned from either API.
-	userMap.set(user.id, user);
+
+	if (user) {
+		// Add current user, since there's no guarantee they are returned from either API.
+		userMap.set(user.id, user);
+	}
 
 	return (
 		<Row>

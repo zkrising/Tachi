8e9256a2263f78a5cc4f792fca7ad474c10e5322
zkldi <20380519+zkldi@users.noreply.github.com>
2022-07-03 22:41:23 +0100

    fix: bootstrap bot properly

--- _scripts/bootstrap.sh
+++ _scripts/bootstrap.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-echo "Moving config files into their right places."
+echo "Moving example config files into usable places."
 
 # https://stackoverflow.com/questions/59895/how-can-i-get-the-directory-where-a-bash-script-is-located-from-within-the-scrip
 # if you actually think bash is a good programming language you are
@@ -14,5 +14,8 @@ cd ..;
 
 cp client/.example.env client/.env
 cp server/test.conf.json5 server/conf.json5
+
+cp bot/example/conf.json5 bot/conf.json5
+cp bot/example/example.env bot/.env
 
 echo "Done."

6f21054aff813cab731c47e2cbc328cae5ff4ed0
zkldi <20380519+zkldi@users.noreply.github.com>
2022-08-21 18:07:56 +0100

    feat: improve sgl output

--- sieglinde/src/main.ts
+++ sieglinde/src/main.ts
@@ -1,52 +1,17 @@
-import SieglindeV0Calc from "./calc/v0";
 import GetTableData from "./fetch-tables";
 import logger from "./logger";
 import SieglindeV1Calc from "calc/v1-jiminp";
-import { Command } from "commander";
 import fs from "fs";
+import path from "path";
+import type { CalcReturns } from "types";
 
 if (require.main !== module) {
 	logger.error(`The script main.ts must be invoked directly!`);
 	process.exit(-1);
 }
 
-const program = new Command();
-
-program
-	.option("-v, --version <The Sieglinde Version to calculate>")
-	.option("-o, --out <Where to output JSON>");
-
-program.parse(process.argv);
-const options: {
-	out?: string;
-	version?: string;
-} = program.opts();
-
-if (!options.out) {
-	logger.error(`Need to provide an --out parameter for output!`);
-	process.exit(-1);
-}
-
-const version = Number(options.version);
-
-if (Number.isNaN(version)) {
-	throw new Error(`Unknown sieglinde version ${version}.`);
-}
-
-function WriteOut(data: string) {
-	fs.writeFileSync(options.out!, data);
-}
-
 void (async () => {
-	let calcFn;
-
-	if (version === 0) {
-		calcFn = SieglindeV0Calc;
-	} else if (version === 1) {
-		calcFn = SieglindeV1Calc;
-	} else {
-		throw new Error(`Unknown sieglinde version ${version}.`);
-	}
+	const calcFn = SieglindeV1Calc;
 
 	fs.mkdirSync(`${__dirname}/cache`, { recursive: true });
 
@@ -54,9 +19,9 @@ void (async () => {
 
 	logger.info(`Starting...`);
 
-	const calcData = [];
-
 	let i = 1;
+
+	fs.mkdirSync(path.join(__dirname, "output"), { recursive: true });
 
 	for (const table of tableInfo) {
 		logger.info(`Running for table ${table.table.name}. ${i}/${tableInfo.length}`);
@@ -65,14 +30,37 @@ void (async () => {
 		// the lr2ir runs off of a toaster which attempts to gut you if you make more than one
 		// request a second.
 		// eslint-disable-next-line no-await-in-loop
-		calcData.push(await calcFn(table));
+		const data = await calcFn(table);
 
-		WriteOut(JSON.stringify(calcData.flat(1)));
+		fs.writeFileSync(
+			path.join(__dirname, `output/${table.table.name}.json`),
+			JSON.stringify(data)
+		);
+
+		fs.writeFileSync(path.join(__dirname, `output/${table.table.name}.csv`), toCSV(data));
 
 		i++;
 	}
 
-	WriteOut(JSON.stringify(calcData.flat(1)));
-
 	logger.info(`Finished!`);
 })();
+
+function toCSV(calcData: Array<CalcReturns>) {
+	let str = `"md5","title","baseLevel","ec","ecStr","hc","hcStr"\n`;
+
+	for (const data of calcData) {
+		const row = [
+			data.md5,
+			data.title.replace(/"/gu, '""'),
+			data.baseLevel,
+			data.ec,
+			data.ecStr,
+			data.hc,
+			data.hcStr,
+		];
+
+		str = `${str}"${row.join('","')}"\n`;
+	}
+
+	return str;
+}

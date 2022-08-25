6fd7324135dc544d48f2d93107919394c4648249
zkldi <20380519+zkldi@users.noreply.github.com>
2022-08-25 23:39:17 +0100

    feat: barbatos sdvx6 support

--- server/src/lib/score-import/import-types/ir/barbatos/converter.ts
+++ server/src/lib/score-import/import-types/ir/barbatos/converter.ts
@@ -3,11 +3,11 @@ import {
 	KTDataNotFoundFailure,
 } from "../../../framework/common/converter-failures";
 import { GenericGetGradeAndPercent } from "../../../framework/common/score-utils";
-import { FindSDVXChartOnInGameID } from "utils/queries/charts";
+import { FindSDVXChartOnInGameIDVersion } from "utils/queries/charts";
 import { FindSongOnID } from "utils/queries/songs";
 import type { DryScore } from "../../../framework/common/types";
 import type { ConverterFunction } from "../../common/types";
-import type { BarbatosContext, BarbatosScore } from "./types";
+import type { BarbatosContext, BarbatosScore, BarbatosSDVX6Score } from "./types";
 import type { Lamps } from "tachi-common";
 
 const LAMP_LOOKUP = {
@@ -28,12 +28,10 @@ const DIFFICULTY_LOOKUP = {
 	4: "MXM",
 };
 
-export const ConverterIRBarbatos: ConverterFunction<BarbatosScore, BarbatosContext> = async (
-	data,
-	context,
-	importType,
-	logger
-) => {
+export const ConverterIRBarbatos: ConverterFunction<
+	BarbatosScore | BarbatosSDVX6Score,
+	BarbatosContext
+> = async (data, context, importType, logger) => {
 	const difficulty = DIFFICULTY_LOOKUP[data.difficulty] as
 		| "ADV"
 		| "ANY_INF"
@@ -41,7 +39,7 @@ export const ConverterIRBarbatos: ConverterFunction<BarbatosScore, BarbatosConte
 		| "MXM"
 		| "NOV";
 
-	const chart = await FindSDVXChartOnInGameID(data.song_id, difficulty);
+	const chart = await FindSDVXChartOnInGameIDVersion(data.song_id, difficulty, context.version);
 
 	if (!chart) {
 		throw new KTDataNotFoundFailure(
@@ -61,9 +59,12 @@ export const ConverterIRBarbatos: ConverterFunction<BarbatosScore, BarbatosConte
 
 	const { percent, grade } = GenericGetGradeAndPercent("sdvx", data.score, chart);
 
+	const { critical, near, miss } = GetJudgements(data);
+	const { fast, slow } = GetFastSlow(data);
+
 	const dryScore: DryScore<"sdvx:Single"> = {
 		game: "sdvx",
-		service: "Barbatos",
+		service: `Barbatos (${context.version})`,
 		comment: null,
 		importType: "ir/barbatos",
 		timeAchieved: context.timeReceived,
@@ -73,21 +74,58 @@ export const ConverterIRBarbatos: ConverterFunction<BarbatosScore, BarbatosConte
 			grade,
 			lamp: LAMP_LOOKUP[data.clear_type] as Lamps["sdvx:Single"],
 			judgements: {
-				critical: data.critical,
-				near: data.near_total,
-				miss: data.error,
+				critical,
+				near,
+				miss,
 			},
 			hitMeta: {
-				fast: data.near_fast,
-				slow: data.near_slow,
+				fast,
+				slow,
 				gauge: data.percent,
 				maxCombo: data.max_chain,
+
+				// only sdvx6 scores store this info
+				exScore: "ex_score" in data ? data.ex_score : null,
 			},
 		},
 		scoreMeta: {
-			inSkillAnalyser: data.is_skill_analyzer,
+			// for some reason, only sdvx5 scores store this info.
+			inSkillAnalyser: "is_skill_analyzer" in data ? data.is_skill_analyzer : null,
 		},
 	};
 
 	return { song, chart, dryScore };
 };
+
+function GetJudgements(data: BarbatosScore | BarbatosSDVX6Score) {
+	if ("ex_score" in data) {
+		// this is a sdvx6 score.
+
+		return {
+			critical: data.early_crit + data.s_crit + data.late_crit,
+			near: data.chip_near,
+			miss: data.early_error + data.late_error,
+		};
+	}
+
+	// otherwise, normal score
+	return {
+		critical: data.critical,
+		near: data.near_total,
+		miss: data.error,
+	};
+}
+
+function GetFastSlow(data: BarbatosScore | BarbatosSDVX6Score) {
+	if ("ex_score" in data) {
+		return {
+			fast: data.early_near,
+			slow: data.late_near,
+		};
+	}
+
+	return {
+		fast: data.near_fast,
+		slow: data.near_slow,
+	};
+}
--- server/src/lib/score-import/import-types/ir/barbatos/parser.ts
+++ server/src/lib/score-import/import-types/ir/barbatos/parser.ts
@@ -2,7 +2,7 @@ import ScoreImportFatalError from "../../../framework/score-importing/score-impo
 import p from "prudence";
 import { FormatPrError } from "utils/prudence";
 import type { ParserFunctionReturns } from "../../common/types";
-import type { BarbatosContext, BarbatosScore } from "./types";
+import type { BarbatosContext, BarbatosScore, BarbatosSDVX6Score } from "./types";
 import type { KtLogger } from "lib/logger/logger";
 
 const PR_BARBATOS = {
@@ -23,21 +23,55 @@ const PR_BARBATOS = {
 	is_skill_analyzer: "boolean",
 };
 
+const PR_BARBATOS_SDVX6 = {
+	difficulty: p.isIn(1, 2, 3, 4),
+	level: p.isBoundedInteger(1, 20),
+	score: p.isBoundedInteger(0, 10_000_000),
+	ex_score: p.isInteger,
+	clear_type: p.isIn(1, 2, 3, 4, 5),
+	song_id: p.isPositiveInteger,
+	grade: p.any,
+	percent: p.isBetween(0, 100),
+	max_chain: p.isPositiveInteger,
+
+	// this is a bit much lmao.
+	early_error: p.isInteger,
+	early_near: p.isInteger,
+	early_crit: p.isInteger,
+	s_crit: p.isInteger,
+	late_crit: p.isInteger,
+	late_near: p.isInteger,
+	late_error: p.isInteger,
+	chip_s_crit: p.isInteger,
+	chip_crit: p.isInteger,
+	chip_near: p.isInteger,
+	chip_error: p.isInteger,
+	long_crit: p.isInteger,
+	long_error: p.isInteger,
+	vol_crit: p.isInteger,
+	vol_error: p.isInteger,
+
+	gauge_type: p.isIn(0, 1, 2, 3),
+};
+
 export function ParseBarbatosSingle(
 	body: Record<string, unknown>,
 	_logger: KtLogger
-): ParserFunctionReturns<BarbatosScore, BarbatosContext> {
-	const err = p(body, PR_BARBATOS);
+): ParserFunctionReturns<BarbatosScore | BarbatosSDVX6Score, BarbatosContext> {
+	// this is an extremely sketchy way of sniffing out whether it's sdvx6 or not.
+	// We could ask barbatos to send a header, but what difference does it make?
+	const schema = "ex_score" in body ? PR_BARBATOS_SDVX6 : PR_BARBATOS;
+
+	const err = p(body, schema);
 
 	if (err) {
 		throw new ScoreImportFatalError(400, FormatPrError(err, "Invalid Barbatos Request"));
 	}
 
-	// asserted using prudence.
 	return {
-		context: { timeReceived: Date.now() },
+		context: { timeReceived: Date.now(), version: "ex_score" in body ? "exceed" : "vivid" },
 		game: "sdvx",
-		iterable: [body] as unknown as Array<BarbatosScore>,
+		iterable: [body] as unknown as Array<BarbatosScore | BarbatosSDVX6Score>,
 		classHandler: null,
 	};
 }

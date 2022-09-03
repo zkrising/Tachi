cbaa9570d545a556cc4d094f1d0eef1e37173d06
zkldi <ktchidev@gmail.com>
2022-09-03 14:29:03 +0100

    fix: bokutachi tab indicators should be blue

--- client/src/components/layout/misc/MaterialThemeProvider.tsx
+++ client/src/components/layout/misc/MaterialThemeProvider.tsx
@@ -1,10 +1,19 @@
 import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import { ColourConfig } from "lib/config";
 import React from "react";
 import { JustChildren } from "types/react";
 
 const theme = createMuiTheme({
 	typography: {
 		fontFamily: "Poppins",
+	},
+	palette: {
+		// for reasons unfathomable to mere mortals
+		// material UI uses "secondary" as the primary colour
+		// for things like tab indicators.
+		secondary: {
+			main: ColourConfig.primary,
+		},
 	},
 });
 

0396d948805ae6b20ec93f9feab65e98a71dc682
zkldi <ktchidev@gmail.com>
2022-09-09 15:08:56 +0100

    fix: remove unused endpoint

--- server/src/server/router/api/v1/games/_game/_playtype/content/bms-router.test.ts
+++ server/src/server/router/api/v1/games/_game/_playtype/content/bms-router.test.ts
@@ -1,18 +1,6 @@
 import t from "tap";
 import mockApi from "test-utils/mock-api";
 
-t.test("GET /api/v1/games/bms/7K/content", (t) => {
-	t.test("Should exist.", async (t) => {
-		const res = await mockApi.get("/api/v1/games/bms/7K/content");
-
-		t.equal(res.body.success, true);
-
-		t.end();
-	});
-
-	t.end();
-});
-
 t.test("GET /api/v1/games/bms/7K/content/sieglindeEC", (t) => {
 	t.test("Should point to the right file.", async (t) => {
 		const res = await mockApi.get("/api/v1/games/bms/7K/content/sieglindeEC/header.json");

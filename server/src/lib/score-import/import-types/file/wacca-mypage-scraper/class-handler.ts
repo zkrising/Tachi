55aefe0b76265b843af2bae0c2c318cc51f8cbb1
Christopher Cooper <christopher.c.cooper@gmail.com>
2022-10-11 19:57:38 -0700

    revamp class-handler

--- server/src/lib/score-import/import-types/file/wacca-mypage-scraper/class-handler.ts
+++ server/src/lib/score-import/import-types/file/wacca-mypage-scraper/class-handler.ts
@@ -1,9 +1,27 @@
 import ScoreImportFatalError from "../../../framework/score-importing/score-import-error";
-import { GetGamePTConfig } from "tachi-common";
+import { WACCA_STAGEUPS } from "lib/constants/classes";
 import type { ClassHandler } from "../../../framework/user-game-stats/types";
 import type { MyPagePlayerStage } from "./types";
 
-const STAGES: Record<number, string> = {
+const STAGES: Record<number, number> = {
+	1: WACCA_STAGEUPS.I,
+	2: WACCA_STAGEUPS.II,
+	3: WACCA_STAGEUPS.III,
+	4: WACCA_STAGEUPS.IV,
+	5: WACCA_STAGEUPS.V,
+	6: WACCA_STAGEUPS.VI,
+	7: WACCA_STAGEUPS.VII,
+	8: WACCA_STAGEUPS.VIII,
+	9: WACCA_STAGEUPS.IX,
+	10: WACCA_STAGEUPS.X,
+	11: WACCA_STAGEUPS.XI,
+	12: WACCA_STAGEUPS.XII,
+	13: WACCA_STAGEUPS.XIII,
+	14: WACCA_STAGEUPS.XIV,
+};
+
+// Keys in this Record should exactly match keys in the STAGES Record.
+const STAGE_NAMES: Record<number, string> = {
 	1: "I",
 	2: "II",
 	3: "III",
@@ -20,15 +38,9 @@ const STAGES: Record<number, string> = {
 	14: "XIV",
 };
 
-const waccaStageUpIds = GetGamePTConfig("wacca", "Single").classHumanisedFormat.stageUp.map(
-	(e) => e.id
-);
-
 export function CreateMyPageScraperClassHandler(stage: MyPagePlayerStage): ClassHandler {
 	return (_game, _playtype, _userID, _ratings, logger) => {
-		// const classSet: GameClassSets["wacca:Single"] = "stageUp";
-
-		const stageName = STAGES[stage.id];
+		const stageName = STAGE_NAMES[stage.id];
 
 		if (stageName === undefined) {
 			throw new ScoreImportFatalError(400, `Unknown stage up id ${stage.id}.`);
@@ -40,9 +52,11 @@ export function CreateMyPageScraperClassHandler(stage: MyPagePlayerStage): Class
 			);
 		}
 
-		const classIndex = waccaStageUpIds.indexOf(stageName);
+		const stageEnum = STAGES[stage.id];
 
-		if (classIndex < 0) {
+		if (stageEnum === undefined) {
+			// If we can find the stage name but not the enum value, something is
+			// seriously wrong. 500 is appropriate.
 			throw new ScoreImportFatalError(
 				500,
 				`Can't find index for stageUp class id ${stageName}.`
@@ -50,7 +64,7 @@ export function CreateMyPageScraperClassHandler(stage: MyPagePlayerStage): Class
 		}
 
 		return {
-			stageUp: classIndex,
+			stageUp: stageEnum,
 		};
 	};
 }

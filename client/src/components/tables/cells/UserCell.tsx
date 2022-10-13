80aa83a99ebaa019735e500ba13648f9406c014d
zkldi <20380519+zkldi@users.noreply.github.com>
2022-10-13 20:43:42 +0100

    feat: display pfps in userCell

--- client/src/components/tables/cells/UserCell.tsx
+++ client/src/components/tables/cells/UserCell.tsx
@@ -1,3 +1,4 @@
+import { ToAPIURL } from "util/api";
 import React from "react";
 import { Link } from "react-router-dom";
 import { PublicUserDocument } from "tachi-common";
@@ -5,8 +6,22 @@ import { GamePT } from "types/react";
 
 export default function UserCell({ user, game, playtype }: { user: PublicUserDocument } & GamePT) {
 	return (
-		<td>
+		<td
+			style={{
+				backgroundImage: user.customPfpLocation
+					? `linear-gradient(to right, rgba(19, 19, 19, 0.8), rgba(19, 19, 19, 1)), url(${ToAPIURL(
+							`/users/${user.id}/pfp`
+					  )})`
+					: undefined,
+				backgroundRepeat: "no-repeat",
+				backgroundSize: "cover",
+				backgroundPosition: "center",
+			}}
+		>
 			<Link
+				style={{
+					maskImage: "unset",
+				}}
 				className="gentle-link"
 				to={`/dashboard/users/${user.username}/games/${game}/${playtype}`}
 			>

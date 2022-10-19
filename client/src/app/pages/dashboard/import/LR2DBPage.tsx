144dfc79b5da02e8e97bc0a74250f94de114f0cb
zkldi <20380519+zkldi@users.noreply.github.com>
2022-10-19 01:17:34 +0100

    fix: lr2hook exists

--- client/src/app/pages/dashboard/import/LR2DBPage.tsx
+++ client/src/app/pages/dashboard/import/LR2DBPage.tsx
@@ -12,12 +12,6 @@ export default function LR2DBPage() {
 			<TISInfo name="LR2 Database" />
 			<Divider />
 			<Muted>
-				This is currently the only way to import scores from LR2, this sucks, because I'd
-				love for scores to automatically import! There is an open bounty on an LR2 automatic
-				score uploader.
-			</Muted>
-			<br />
-			<Muted>
 				Note: If you submit a score on a chart that {TachiConfig.name} doesn't recognise,
 				you'll need to wait until atleast 2 other players submit scores for that chart
 				before it'll show up. This is to combat accidental IR spam.

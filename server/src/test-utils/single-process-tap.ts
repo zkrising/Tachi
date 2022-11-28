73f30f9f46a42163afeb39ad4c356ce21f89be40
ereti <43418802+ereti@users.noreply.github.com>
2022-11-28 01:04:33 +0000

    fix: tests

--- server/src/test-utils/single-process-tap.ts
+++ server/src/test-utils/single-process-tap.ts
@@ -6,7 +6,7 @@ import path from "path";
 
 const files = glob.sync(path.join(__dirname, "../../", "**/*.test.ts"));
 
-process.env.NODE_PATH = path.join(__dirname, "../../");
+process.env.NODE_PATH = path.join(__dirname, "../");
 
 for (const file of files) {
 	// Deliberate -- we're doing hackery here.

b48b6e37bb3c5077f594750d4023453fa3631b68
zkldi <20380519+zkldi@users.noreply.github.com>
2022-12-14 01:47:30 +0000

    fix: require permissions to add people lol

--- server/src/server/router/api/v1/users/_userID/following/router.ts
+++ server/src/server/router/api/v1/users/_userID/following/router.ts
@@ -1,3 +1,4 @@
+import { RequireSelfRequestFromUser } from "../middleware";
 import { Router } from "express";
 import db from "external/mongo/db";
 import CreateLogCtx from "lib/logger/logger";
@@ -51,74 +52,78 @@ router.get("/", async (req, res) => {
  *
  * @name POST /api/v1/users/:userID/following/add
  */
-router.post("/add", prValidate({ userID: p.isPositiveInteger }), async (req, res) => {
-	const user = GetUser(req);
+router.post(
+	"/add",
+	RequireSelfRequestFromUser.prValidate({ userID: p.isPositiveInteger }),
+	async (req, res) => {
+		const user = GetUser(req);
 
-	const { userID: toFollow } = req.safeBody as { userID: integer };
+		const { userID: toFollow } = req.safeBody as { userID: integer };
 
-	if (user.id === toFollow) {
-		return res.status(400).json({
-			success: false,
-			description: `Can't follow yourself. Bit self-indulgent!`,
-		});
-	}
-
-	const settings = await db["user-settings"].findOne({ userID: user.id });
-
-	if (!settings) {
-		logger.error(`User ${FormatUserDoc(user)} has no settings?`, { user });
-
-		return res.status(500).json({
-			success: false,
-			description: `This user has no settings.`,
-		});
-	}
-
-	if (settings.following.includes(toFollow)) {
-		return res.status(409).json({
-			success: false,
-			description: `You are already following this user.`,
-		});
-	}
-
-	if (settings.following.length >= ServerConfig.MAX_FOLLOWING_AMOUNT) {
-		return res.status(400).json({
-			success: false,
-			description: `You are following too many people. The max is ${ServerConfig.MAX_FOLLOWING_AMOUNT}.`,
-		});
-	}
-
-	const userToFollow = await GetUserWithID(toFollow);
-
-	if (!userToFollow) {
-		return res.status(400).json({
-			success: false,
-			description: `No user with the id '${toFollow}' exists.`,
-		});
-	}
-
-	// Instead of using $push in mongodb, we create a new array and set that.
-	// due to the above guard, it's not possible for this to ever result
-	// in a race condition.
-	const following = [...settings.following, toFollow];
-
-	await db["user-settings"].update(
-		{
-			userID: user.id,
-		},
-		{
-			$set: {
-				following,
-			},
+		if (user.id === toFollow) {
+			return res.status(400).json({
+				success: false,
+				description: `Can't follow yourself. Bit self-indulgent!`,
+			});
 		}
-	);
 
-	return res.status(200).json({
-		success: true,
-		description: `Added ${userToFollow.username}.`,
-		body: {},
-	});
-});
+		const settings = await db["user-settings"].findOne({ userID: user.id });
+
+		if (!settings) {
+			logger.error(`User ${FormatUserDoc(user)} has no settings?`, { user });
+
+			return res.status(500).json({
+				success: false,
+				description: `This user has no settings.`,
+			});
+		}
+
+		if (settings.following.includes(toFollow)) {
+			return res.status(409).json({
+				success: false,
+				description: `You are already following this user.`,
+			});
+		}
+
+		if (settings.following.length >= ServerConfig.MAX_FOLLOWING_AMOUNT) {
+			return res.status(400).json({
+				success: false,
+				description: `You are following too many people. The max is ${ServerConfig.MAX_FOLLOWING_AMOUNT}.`,
+			});
+		}
+
+		const userToFollow = await GetUserWithID(toFollow);
+
+		if (!userToFollow) {
+			return res.status(400).json({
+				success: false,
+				description: `No user with the id '${toFollow}' exists.`,
+			});
+		}
+
+		// Instead of using $push in mongodb, we create a new array and set that.
+		// due to the above guard, it's not possible for this to ever result
+		// in a race condition.
+		const following = [...settings.following, toFollow];
+
+		await db["user-settings"].update(
+			{
+				userID: user.id,
+			},
+			{
+				$set: {
+					following,
+				},
+			}
+		);
+
+		return res.status(200).json({
+			success: true,
+			description: `Added ${userToFollow.username}.`,
+			body: {},
+		});
+	}
+);
 
 /**
  * Unfollow a user.
@@ -127,59 +132,64 @@ router.post("/add", prValidate({ userID: p.isPositiveInteger }), async (req, res
  *
  * @name POST /api/v1/users/:userID/following/remove
  */
-router.post("/remove", prValidate({ userID: p.isPositiveInteger }), async (req, res) => {
-	const user = GetUser(req);
+router.post(
+	"/remove",
+	RequireSelfRequestFromUser,
+	prValidate({ userID: p.isPositiveInteger }),
+	async (req, res) => {
+		const user = GetUser(req);
 
-	const { userID: toFollow } = req.safeBody as { userID: integer };
+		const { userID: toFollow } = req.safeBody as { userID: integer };
 
-	const settings = await db["user-settings"].findOne({ userID: user.id });
+		const settings = await db["user-settings"].findOne({ userID: user.id });
 
-	if (!settings) {
-		logger.error(`User ${FormatUserDoc(user)} has no settings?`, { user });
+		if (!settings) {
+			logger.error(`User ${FormatUserDoc(user)} has no settings?`, { user });
 
-		return res.status(500).json({
-			success: false,
-			description: `This user has no settings.`,
-		});
-	}
-
-	if (!settings.following.includes(toFollow)) {
-		return res.status(409).json({
-			success: false,
-			description: `You are not following this user.`,
-		});
-	}
-
-	const userToFollow = await GetUserWithID(toFollow);
-
-	if (!userToFollow) {
-		return res.status(400).json({
-			success: false,
-			description: `No user with the id '${toFollow}' exists.`,
-		});
-	}
-
-	// Instead of using $pull in mongodb, we create a new array and set that.
-	// due to the above guard, it's not possible for this to ever result
-	// in a race condition.
-	const following = settings.following.filter((e) => e !== toFollow);
-
-	await db["user-settings"].update(
-		{
-			userID: user.id,
-		},
-		{
-			$set: {
-				following,
-			},
+			return res.status(500).json({
+				success: false,
+				description: `This user has no settings.`,
+			});
 		}
-	);
 
-	return res.status(200).json({
-		success: true,
-		description: `Unfollowed ${userToFollow.username}.`,
-		body: {},
-	});
-});
+		if (!settings.following.includes(toFollow)) {
+			return res.status(409).json({
+				success: false,
+				description: `You are not following this user.`,
+			});
+		}
+
+		const userToFollow = await GetUserWithID(toFollow);
+
+		if (!userToFollow) {
+			return res.status(400).json({
+				success: false,
+				description: `No user with the id '${toFollow}' exists.`,
+			});
+		}
+
+		// Instead of using $pull in mongodb, we create a new array and set that.
+		// due to the above guard, it's not possible for this to ever result
+		// in a race condition.
+		const following = settings.following.filter((e) => e !== toFollow);
+
+		await db["user-settings"].update(
+			{
+				userID: user.id,
+			},
+			{
+				$set: {
+					following,
+				},
+			}
+		);
+
+		return res.status(200).json({
+			success: true,
+			description: `Unfollowed ${userToFollow.username}.`,
+			body: {},
+		});
+	}
+);
 
 export default router;

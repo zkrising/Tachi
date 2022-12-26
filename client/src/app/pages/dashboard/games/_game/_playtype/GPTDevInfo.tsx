99352f2c5a09f7fec51b2c1b9a18f5ee37516c51
zkldi <20380519+zkldi@users.noreply.github.com>
2022-12-26 17:14:14 +0000

    feat: class badge visualiser

--- client/src/app/pages/dashboard/games/_game/_playtype/GPTDevInfo.tsx
+++ client/src/app/pages/dashboard/games/_game/_playtype/GPTDevInfo.tsx
@@ -1,8 +1,11 @@
+import ClassBadge from "components/game/ClassBadge";
 import useSetSubheader from "components/layout/header/useSetSubheader";
 import Card from "components/layout/page/Card";
+import MiniTable from "components/tables/components/MiniTable";
 import DebugContent from "components/util/DebugContent";
 import React from "react";
-import { FormatGame, GetGameConfig, GetGamePTConfig } from "tachi-common";
+import { FormatGame, GetGameConfig, GetGamePTConfig, IDStrings, gameClasses } from "tachi-common";
+import { GameClassSets } from "tachi-common/game-classes";
 import { GamePT } from "types/react";
 
 export default function GPTDevInfo({ game, playtype }: GamePT) {
@@ -22,6 +25,29 @@ export default function GPTDevInfo({ game, playtype }: GamePT) {
 			</Card>
 			<Card className="mt-4" header="GPT Configuration">
 				<DebugContent data={gptConfig} />
+			</Card>
+			<Card className="mt-4" header="Class Badges">
+				<div className="d-flex w-100 justify-content-center" style={{ gap: "30px" }}>
+					{Object.entries(gptConfig.classHumanisedFormat).map(([classSet, value]) => (
+						<div>
+							<MiniTable headers={[classSet]} colSpan={2}>
+								{value.map((e, i) => (
+									<tr key={e.id}>
+										<td>{e.id}</td>
+										<td>
+											<ClassBadge
+												classSet={classSet as GameClassSets[IDStrings]}
+												classValue={i}
+												game={game}
+												playtype={playtype}
+											/>
+										</td>
+									</tr>
+								))}
+							</MiniTable>
+						</div>
+					))}
+				</div>
 			</Card>
 		</>
 	);

e8a51c1b38a917ee2fe820e2dc965adbca54df95
zkldi <20380519+zkldi@users.noreply.github.com>
2023-01-26 13:05:32 +0000

    fix: silent dl

--- client/src/app/pages/dashboard/import/SilentHookPage.tsx
+++ client/src/app/pages/dashboard/import/SilentHookPage.tsx
@@ -21,7 +21,7 @@ export default function SilentHookPage() {
 			<ol className="instructions-list">
 				<li>
 					Download <code>silent</code> from{" "}
-					<ExternalLink href="https://cdn.discordapp.com/attachments/783119769649414166/1067870352291672064/Release.zip">
+					<ExternalLink href="https://cdn.discordapp.com/attachments/687614263287611434/1068130139755528232/Release.zip">
 						Here
 					</ExternalLink>
 					and place all the <code>.dll</code> files in the same folder as{" "}

085d1789b4703080a50758734a82aba625bd0d43
zkldi <20380519+zkldi@users.noreply.github.com>
2023-02-27 23:38:43 +0000

    feat: new, less efficient, better search engine

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,14 @@
+/* eslint-disable import/first */
 // Before we run anything, set a global to indicate to the code that
 // we're running as a server, and not as a job runner or score worker.
+process.env.IS_SERVER = "true";
+
+// see: https://www.npmjs.com/package/fix-esm
+// fzf *mandates* esm, but we don't use it (ts transpiles to cjs)
+// sick of this headache.
+// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
+require("fix-esm").register();
+
 import db, { monkDB } from "external/mongo/db";
 import { UpdateIndexes } from "external/mongo/indexes";
 import { InitSequenceDocs } from "external/mongo/sequence-docs";
@@ -17,8 +26,6 @@ import fs from "fs";
 import https from "https";
 import path from "path";
 import type http from "http";
-
-process.env.IS_SERVER = "true";
 
 const logger = CreateLogCtx(__filename);
 

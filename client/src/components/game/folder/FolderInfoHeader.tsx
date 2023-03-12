f76b1eec7ed6dca345b6dfca7125e7d7875aa179
zkldi <20380519+zkldi@users.noreply.github.com>
2023-03-12 15:47:34 +0000

    feat: rough folder avgs

--- client/src/components/game/folder/FolderInfoHeader.tsx
+++ client/src/components/game/folder/FolderInfoHeader.tsx
@@ -4,12 +4,14 @@ import Divider from "components/util/Divider";
 import Icon from "components/util/Icon";
 import SelectButton from "components/util/SelectButton";
 import { useBucket } from "components/util/useBucket";
-import React, { useState } from "react";
-import { GetGamePTConfig, GetScoreEnumConfs, UserDocument } from "tachi-common";
+import React, { useMemo, useState } from "react";
+import { GPTString, GetGamePTConfig, GetScoreEnumConfs, UserDocument } from "tachi-common";
 import { GamePT } from "types/react";
 import { FolderDataset } from "types/tables";
+import { GPT_CLIENT_IMPLEMENTATIONS } from "lib/game-implementations";
 import FolderMinimap from "./FolderMinimap";
 import FolderScoreDistributionChart from "./FolderScoreDistributionChart";
+import FolderScoreAverages from "./FolderScoreAverages";
 
 export default function FolderInfoHeader({
 	game,
@@ -23,53 +25,67 @@ export default function FolderInfoHeader({
 	folderTitle: string;
 } & GamePT) {
 	const preferredDefaultEnum = useBucket(game, playtype);
-	const [enumMetric, setEnumMetric] = useState<string>(preferredDefaultEnum);
-	const [breakdownInfo, setBreakdownInfo] = useState<"graph" | "switchboard">("switchboard");
+
+	const [currentGraph, setCurrentGraph] = useState<string>(`${preferredDefaultEnum}-stats`);
+
+	const enumGraphs = ["minimap", "stats"];
 
 	const gptConfig = GetGamePTConfig(game, playtype);
+	const gptImpl = GPT_CLIENT_IMPLEMENTATIONS[`${game}:${playtype}` as GPTString];
 
 	const enumConf = GetScoreEnumConfs(gptConfig);
+
+	const [metric, type] = useMemo(() => currentGraph.split("-"), [currentGraph]);
 
 	return (
 		<Card header={`${reqUser.username}'s ${folderTitle} Breakdown`}>
 			<div className="col-12 d-flex justify-content-center">
 				<div className="btn-group">
-					{Object.keys(enumConf).map((k) => (
-						<SelectButton value={enumMetric} setValue={setEnumMetric} id={k}>
-							<Icon type="chart-area" />
-							{UppercaseFirst(k)} Info
-						</SelectButton>
-					))}
+					{enumGraphs.flatMap((g) =>
+						Object.keys(enumConf).flatMap((en) => (
+							<SelectButton
+								className={g === "minimap" ? "d-none d-lg-block" : ""}
+								value={currentGraph}
+								setValue={setCurrentGraph}
+								id={`${en}-${g}`}
+							>
+								{/* @ts-expect-error zzz */}
+								<Icon type={gptImpl.enumIcons[en]} />
+								{UppercaseFirst(en)} {UppercaseFirst(g)}
+							</SelectButton>
+						))
+					)}
+					<SelectButton value={currentGraph} setValue={setCurrentGraph} id="score">
+						<Icon type="sort" />
+						Score Averages
+					</SelectButton>
 				</div>
-			</div>
-			<div className="col-12 mt-4 d-flex justify-content-center">
-				<select
-					className="form-control"
-					value={breakdownInfo}
-					onChange={(e) => setBreakdownInfo(e.target.value as any)}
-				>
-					<option value="graph">Thermometer</option>
-					<option value="switchboard">Switchboard</option>
-				</select>
 			</div>
 			<div className="col-12">
 				<Divider />
 			</div>
 
-			{breakdownInfo === "graph" ? (
+			{type === "stats" ? (
 				<FolderScoreDistributionChart
 					game={game}
 					folderDataset={folderDataset}
 					playtype={playtype}
-					view={enumMetric}
+					view={metric}
 				/>
-			) : (
+			) : type === "minimap" ? (
 				<FolderMinimap
 					reqUser={reqUser}
 					game={game}
 					playtype={playtype}
 					folderDataset={folderDataset}
-					enumMetric={enumMetric}
+					enumMetric={metric}
+				/>
+			) : (
+				<FolderScoreAverages
+					reqUser={reqUser}
+					game={game}
+					playtype={playtype}
+					folderDataset={folderDataset}
 				/>
 			)}
 		</Card>

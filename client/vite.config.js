d3e109f34a7736cc18cbdb0edc417f3801a9b8ed
zkldi <20380519+zkldi@users.noreply.github.com>
2023-03-22 23:43:35 +0000

    fix: scss refactor

--- client/vite.config.js
+++ client/vite.config.js
@@ -44,11 +44,11 @@ export default defineConfig(() => {
 
 	// If you have access to the private metronic scss submodule
 	// and have cloned it, use that.
-	if (fs.existsSync(path.join(__dirname, "../_assets/metronic-scss/style.react.scss"))) {
-		scssEntryPoint = "../_assets/metronic-scss/style.react.scss";
+	if (fs.existsSync(path.join(__dirname, "./src/_assets/metronic-scss/style.react.scss"))) {
+		scssEntryPoint = "./src/_assets/metronic-scss/style.react.scss";
 	} else {
 		// Else, use some default compiled css.
-		scssEntryPoint = "../_assets/compiled-css/main.css";
+		scssEntryPoint = './src/_assets/compiled-css/main.css";@import "./src/_style/base.scss';
 	}
 
 	return {

a15432d9935ee2f3f51c75a8e34f392f224a21c8
zkldi <20380519+zkldi@users.noreply.github.com>
2023-03-24 00:03:19 +0000

    fix: local man makes typo

--- client/src/components/activity/Activity.tsx
+++ client/src/components/activity/Activity.tsx
@@ -705,7 +705,7 @@ function UGPTLink({ reqUser, game, playtype }: UGPT) {
 			}}
 		>
 			{user?.id === reqUser.id ? "You" : reqUser.username}
-			{user?.isSupporter ? (
+			{reqUser?.isSupporter ? (
 				<>
 					{" "}
 					<SupporterIcon />

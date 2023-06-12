4aa89029bb3e653bd40c7069f212d0d1b11ae0da
saintnoodle <14948290+saintnoodle@users.noreply.github.com>
2023-06-12 23:41:00 +0000

    react-bootstrap button with useHistory

--- client/src/components/util/LinkButton.tsx
+++ client/src/components/util/LinkButton.tsx
@@ -1,29 +1,36 @@
-import React, { CSSProperties } from "react";
-import { Link } from "react-router-dom";
-import { JustChildren } from "types/react";
+import React from "react";
+import Button, { ButtonProps } from "react-bootstrap/Button";
+import { useHistory } from "react-router-dom";
 
-export default function LinkButton({
-	to,
-	className = "btn-primary",
-	children,
-	style,
-	onClick,
-	disabled = false,
-}: {
+interface LinkButtonProps extends ButtonProps {
 	to: string;
-	className?: string;
-	style?: CSSProperties;
-	onClick?: () => void;
-	disabled?: boolean;
-} & JustChildren) {
+	children?: React.ReactNode;
+}
+
+/**Use as a react-bootstrap \<Button\> and pass a path (to) to navigate to with useHistory*/
+export default function LinkButton({ to, children, ...props }: LinkButtonProps) {
+	const history = useHistory();
+	function handleClick(e: React.MouseEvent<HTMLButtonElement>) {
+		if (
+			// Allow link to work normally under these conditions
+			!e.defaultPrevented &&
+			e.button === 0 &&
+			(!props.target || props.target === "_self") &&
+			!isModified(e)
+		) {
+			// Conditions above are satisfied, navigate to path programatically
+			e.preventDefault();
+			history.push(to);
+		}
+	}
+
+	function isModified(e: React.MouseEvent<HTMLButtonElement>) {
+		return !!(e.metaKey || e.altKey || e.ctrlKey || e.shiftKey);
+	}
+
 	return (
-		<Link
-			onClick={onClick}
-			to={to}
-			className={`btn ${disabled ? "disabled" : ""} ${className}`}
-			style={style}
-		>
+		<Button as="a" href={to} onClick={(e) => handleClick(e)} {...props}>
 			{children}
-		</Link>
+		</Button>
 	);
 }

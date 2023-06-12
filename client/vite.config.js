2167896d363b79974c61b49a2db81dac6425bcb8
saintnoodle <14948290+saintnoodle@users.noreply.github.com>
2023-06-12 01:47:47 +0000

    allow loading env vars from .env.{mode}

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,27 +1,22 @@
 import react from "@vitejs/plugin-react";
-import { config } from "dotenv";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
 import fs from "fs";
 import path from "path";
-
-config();
 
 const toplevelDirs = fs
 	.readdirSync(path.join(__dirname, "src/"), { withFileTypes: true })
 	.filter((e) => e.isDirectory())
 	.map((e) => e.name);
 
-export default defineConfig(() => {
-	// Determines which stylesheet gets loaded
-	let styleMode;
+export default defineConfig(({ mode }) => {
+	// load .env.{mode} (.env & .env.development or .env.production by default)
+	const env = loadEnv(mode, process.cwd(), "");
+	// Determines which stylesheet gets loaded, kamai by default.
+	let styleMode = `./src/_style/tachi/colors/kamai.scss`;
 
-	if (process.env.VITE_TCHIC_MODE === "ktchi" || process.env.VITE_TCHIC_MODE === "omni") {
-		// kamai colours
-		styleMode = `./src/_style/tachi/colors/kamai.scss`;
-	} else {
-		// boku colours
-		styleMode = `./src/_style/tachi/colors/boku.scss`
+	if (env.VITE_TCHIC_MODE === "btchi") {
+		styleMode = `./src/_style/tachi/colors/boku.scss`;
 	}
 
 	return {
@@ -37,21 +32,25 @@ export default defineConfig(() => {
 				},
 				{
 					find: /@fonts/u,
-					replacement: process.env.VITE_IS_LOCAL_DEV ? path.resolve(__dirname, "src", "_assets", "fonts") : "./static/fonts",
+					replacement: env.VITE_IS_LOCAL_DEV
+						? path.resolve(__dirname, "src", "_assets", "fonts")
+						: "./static/fonts",
 				},
 			],
 		},
 		define: {
-			"process.env": process.env,
+			"process.env": env,
 		},
 		plugins: [
 			react(),
 			createHtmlPlugin({
 				inject: {
 					data: {
-						VITE_CDN_URL: process.env.VITE_CDN_URL,
-						TACHI_NAME: process.env.TACHI_NAME ?? "Tachi",
-						VITE_ADDITIONAL_HEAD: process.env.VITE_GOATCOUNTER ? `<script data-goatcounter="${process.env.VITE_GOATCOUNTER}" async src="//gc.zgo.at/count.js"></script>` : ""
+						VITE_CDN_URL: env.VITE_CDN_URL,
+						TACHI_NAME: env.TACHI_NAME ?? "Tachi",
+						VITE_ADDITIONAL_HEAD: env.VITE_GOATCOUNTER
+							? `<script data-goatcounter="${env.VITE_GOATCOUNTER}" async src="//gc.zgo.at/count.js"></script>`
+							: "",
 					},
 				},
 			}),
@@ -60,8 +59,8 @@ export default defineConfig(() => {
 			port: 3000,
 			host: true,
 			watch: {
-				usePolling: process.env.FORCE_FS_POLLING,
-			}
+				usePolling: env.FORCE_FS_POLLING,
+			},
 		},
 		preview: {
 			port: 3000,
@@ -76,7 +75,7 @@ export default defineConfig(() => {
 		build: {
 			assetsDir: "static",
 			// eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-			outDir: process.env.BUILD_OUT_DIR || "build",
+			outDir: env.BUILD_OUT_DIR || "build",
 			sourcemap: true,
 		},
 	};

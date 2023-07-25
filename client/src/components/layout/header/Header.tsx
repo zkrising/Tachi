0b3b7dcc1b3baf6361916f59d605bd32e1ac707b
saintnoodle <14948290+saintnoodle@users.noreply.github.com>
2023-07-26 00:42:09 +0100

    feat: bootstrap 5 offcanvas nav header
    this new header uses bootstrap's new offcanvas nav component
    
    it also recieves a face lift using BS 5's utility api for backdrop blur

--- client/src/components/layout/header/Header.tsx
+++ client/src/components/layout/header/Header.tsx
@@ -1,36 +1,64 @@
-import { ToCDNURL } from "util/api";
-import React from "react";
+import useBreakpoint from "util/useBreakpoint";
+import { Breakpoints } from "util/constants/breakpoints";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { SetState } from "types/react";
+import Container from "react-bootstrap/Container";
+import { Navbar, Offcanvas, OffcanvasHeader } from "react-bootstrap";
+import SiteWordmark from "components/util/SiteWordmark";
+import SignOut from "components/util/SignOut";
+import { UserContext } from "context/UserContext";
 import { HeaderMenu } from "./HeaderMenu";
-import { Topbar } from "./Topbar";
+import UserArea from "./UserArea";
+import MobileMenuToggle from "./MobileMenuToggle";
+import Logo from "./Logo";
 
-export function Header({
-	mobileShow,
-	setMobileShow,
-}: {
-	mobileShow: boolean;
-	setMobileShow: SetState<boolean>;
-}) {
+export default function Header() {
+	const { user } = useContext(UserContext);
+	const [showMobileMenu, setShowMobileMenu] = useState(false);
+	const breakpoint = useBreakpoint();
+
+	const dropdownMenuStyle =
+		breakpoint > Breakpoints.md ? { transform: "translateY(16px)" } : undefined;
+
+	useEffect(() => {
+		if (breakpoint >= Breakpoints.md) {
+			setShowMobileMenu(false);
+		}
+	}, [breakpoint]);
 	return (
-		<div className="header header-fixed" id="kt_header">
-			<div className="container d-flex align-items-stretch justify-content-between">
-				<div className="d-flex align-items-stretch me-3">
-					<div className="header-logo">
-						<Link to="/">
-							<img
-								className="logo-default max-h-40px"
-								alt="Logo"
-								src={ToCDNURL("/logos/logo-mark.png")}
-							/>
-						</Link>
-					</div>
-
-					<HeaderMenu mobileShow={mobileShow} setMobileShow={setMobileShow} />
-				</div>
-
-				<Topbar />
-			</div>
-		</div>
+		<header
+			id="main-header"
+			className="bg-body bg-opacity-75 backdrop-blur-xl border-bottom border-secondary border-opacity-25 fixed-top"
+		>
+			<Navbar expand={"md"} variant="" className="h-100 p-0">
+				<Container className="d-flex align-items-center">
+					<Logo />
+					<MobileMenuToggle state={showMobileMenu} setState={setShowMobileMenu} />
+					<Navbar.Offcanvas
+						id="mobile-menu"
+						aria-labelledby="mobile-menu-label"
+						show={showMobileMenu}
+						onHide={() => setShowMobileMenu(false)}
+					>
+						<OffcanvasHeader className="p-4 border-bottom border-secondary border-opacity-25">
+							<Link
+								id="home"
+								to="/"
+								className="mx-auto p-2 focus-ring focus-ring-light"
+							>
+								<SiteWordmark id="mobile-menu-label" width="192px" />
+							</Link>
+						</OffcanvasHeader>
+						<Offcanvas.Body className="ps-lg-4 h-lg-100 position-relative">
+							<HeaderMenu user={user} dropdownMenuStyle={dropdownMenuStyle} />
+							<div className="position-absolute bottom-0 w-100 pb-4 px-4 d-md-none">
+								<SignOut className="w-100 bg-body-secondary bg-opacity-75 backdrop-blur-xl" />
+							</div>
+						</Offcanvas.Body>
+					</Navbar.Offcanvas>
+					<UserArea user={user} dropdownMenuStyle={dropdownMenuStyle} />
+				</Container>
+			</Navbar>
+		</header>
 	);
 }

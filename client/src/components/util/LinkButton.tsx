c81399b584ba2aec4ba007d03dd81cc20b3126f7
saintnoodle <14948290+saintnoodle@users.noreply.github.com>
2023-07-25 21:47:54 +0100

    feat: react-router-bootstrap
    a react-bootstrap library that wraps components and adds react router props
    
    also included are some types for react-bootstrap

--- client/src/components/util/LinkButton.tsx
+++ client/src/components/util/LinkButton.tsx
@@ -1,29 +1,33 @@
-import React, { CSSProperties } from "react";
-import { Link } from "react-router-dom";
-import { JustChildren } from "types/react";
+import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import Button, { ButtonProps } from "react-bootstrap/Button";
+import { LinkContainerProps } from "types/bootstrap";
 
+/**A Bootstrap Button component that acts like a react-router Link */
 export default function LinkButton({
 	to,
-	className = "btn-primary",
+	activeClassName,
+	activeStyle,
+	isActive,
+	replace,
+	state,
 	children,
 	style,
-	onClick,
-	disabled = false,
-}: {
-	to: string;
-	className?: string;
-	style?: CSSProperties;
-	onClick?: () => void;
-	disabled?: boolean;
-} & JustChildren) {
+	className,
+	...props
+}: LinkContainerProps & ButtonProps) {
 	return (
-		<Link
-			onClick={onClick}
+		<LinkContainer
 			to={to}
-			className={`btn ${disabled ? "disabled" : ""} ${className}`}
+			activeClassName={activeClassName}
+			activeStyle={activeStyle}
+			replace={replace}
+			state={state}
+			isActive={isActive}
 			style={style}
+			className={className}
 		>
-			{children}
-		</Link>
+			<Button {...props}>{children}</Button>
+		</LinkContainer>
 	);
 }

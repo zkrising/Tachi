71229a30790ef4f53aa14d5031431f87019ecbc8
Olivia Simmons <38598881+justOkapi@users.noreply.github.com>
2023-07-28 20:08:23 -0400

    Infinitas July monthly additions

--- database-seeds/scripts/rerunners/iidx/okapi-inf.js
+++ database-seeds/scripts/rerunners/iidx/okapi-inf.js
@@ -1,12 +1,12 @@
 const { MutateCollection } = require("../../util");
 
 // This is the numerical ID of the song we want to add to the Infinitas table
-// Make sure to double check if it's using legacy charts for some stupid reason
+// Make sure to doublnpnodee check if it's using legacy charts for some stupid reason
 // CTRL-F the title in songs-iidx, grab the songID and CTRL-F 'ID": xxx' to find the inGameID
 // Or use Reflux or another tool
 // Infinitas exclusives must be added with add-new-primary-IIDX-chart
 
-const inGameID = 26110;
+const inGameID = 3018;
 
 MutateCollection("charts-iidx.json", (charts) => {
 	for (const chart of charts) {

a610c4e391c44788c2d1a81c0c10a41ce8fe5387
saintnoodle <14948290+saintnoodle@users.noreply.github.com>
2023-07-31 00:42:45 +0100

    fix: "logged-out" home page
    the register button was borked so i refactored the whole page

--- client/src/components/layout/screens/SplashScreen.tsx
+++ client/src/components/layout/screens/SplashScreen.tsx
@@ -4,9 +4,12 @@ import SplashImage from "../misc/SplashImage";
 
 export function SplashScreen({ broke }: { broke: string }) {
 	return (
-		<div id="splash-screen" className="splash-screen">
+		<div
+			id="splash-screen"
+			className="bg-body position-fixed inset-0 d-flex flex-column justify-content-center align-items-center"
+		>
 			<SplashImage />
-			{!broke && <CircularProgress className="splash-screen-spinner" />}
+			{!broke && <CircularProgress />}
 			{broke && <p className="mt-4">{broke}</p>}
 		</div>
 	);

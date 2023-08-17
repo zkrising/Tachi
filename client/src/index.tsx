0a247956d81cf71b32f91920a8f55de3776cee0f
zkldi <20380519+zkldi@users.noreply.github.com>
2023-08-17 19:27:22 +0100

    test commit

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,4 +6,6 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const { PUBLIC_URL } = process.env;
 
+const string: string = 5;
+
 ReactDOM.render(<App basename={PUBLIC_URL!} />, document.getElementById("root"));

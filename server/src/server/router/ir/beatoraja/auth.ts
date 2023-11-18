340abe3dcc875c680212443453ed370e86b0b89f
zkldi <20380519+zkldi@users.noreply.github.com>
2023-11-18 17:40:10 +0000

    feat: support tachiIR v3

--- server/src/server/router/ir/beatoraja/auth.ts
+++ server/src/server/router/ir/beatoraja/auth.ts
@@ -3,7 +3,7 @@ import type { RequestHandler } from "express";
 export const ValidateIRClientVersion: RequestHandler = (req, res, next) => {
 	const header = req.header("X-TachiIR-Version");
 
-	if (!header?.startsWith("v2")) {
+	if (!header?.startsWith("v2") && !header?.startsWith("v3")) {
 		return res.status(400).json({
 			success: false,
 			description: `Invalid X-TachiIR-Version.`,

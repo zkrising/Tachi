393c205e003a7e2ca89d20169dd327c59fc96e36
saintnoodle <14948290+saintnoodle@users.noreply.github.com>
2023-12-10 19:34:13 +0000

    fix: header overflow
    fixes a bug introduced by a previous "fix"

--- client/src/components/layout/header/HeaderMenu.tsx
+++ client/src/components/layout/header/HeaderMenu.tsx
@@ -41,10 +41,7 @@ export function HeaderMenu({
 	}, [error, data]);
 
 	return (
-		<Nav
-			as="nav"
-			className="p-4 d-flex align-content-between gap-4 h-100 overflow-y-auto overflow-x-hidden"
-		>
+		<Nav as="nav" className="p-4 d-flex align-content-between gap-4 h-100">
 			{user && ugs && ugs.length !== 0 && (
 				<UGPTDropdown
 					user={user}

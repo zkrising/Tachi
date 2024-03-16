562648b2f771a2ef9b4ac854075f1029357e3974
Olivia Simmons <just.okapi@gmail.com>
2024-03-15 23:02:55 -0400

    updated Infinitas script to take inGameID as CLI args

--- database-seeds/scripts/rerunners/iidx/mina-inf.js
+++ database-seeds/scripts/rerunners/iidx/mina-inf.js
@@ -6,19 +6,44 @@ const { MutateCollection } = require("../../util");
 // Or use a tool capable of extracting is (ie Reflux)
 // Infinitas exclusives must be added with add-new-primary-IIDX-chart
 // This script is ONLY for regular charts, DO NOT USE FOR LEGGENDARIAS
+// To use, run 'node .\mina-inf.js --<ingameid 1> --<ingameid 2> [...] --<ingameid thelastoneyouhave>'
 
-const inGameID = 5028;
+// Extract the command line arguments
+const args = process.argv.slice(2);
+
+// Check if inGameIDs are provided as command line arguments
+const inGameIDs = [];
+for (let i = 0; i < args.length; i++) {
+	if (args[i].startsWith("--")) {
+		inGameIDs.push(parseInt(args[i].substring(2), 10));
+	} else {
+		console.error("Invalid argument format:", args[i]);
+		console.error("Please provide inGameIDs as command line arguments like this: --<inGameID>");
+		process.exit(1);
+	}
+}
+
+// Check if at least one inGameID is provided
+if (inGameIDs.length === 0) {
+	console.error(
+		"Please provide at least one inGameID as a command line argument like this: --<inGameID>"
+	);
+	process.exit(1);
+}
 
 MutateCollection("charts-iidx.json", (charts) => {
-	for (const chart of charts) {
-		if (
-			chart.data.inGameID === inGameID &&
-			!chart.versions.includes("inf") &&
-			chart.data["2dxtraSet"] === null &&
-			chart.difficulty !== "LEGGENDARIA" &&
-			chart.difficulty !== "BEGINNER"
-		) {
-			chart.versions.push("inf");
+	// Iterate over each provided inGameID
+	for (const inGameID of inGameIDs) {
+		for (const chart of charts) {
+			if (
+				chart.data.inGameID === inGameID &&
+				!chart.versions.includes("inf") &&
+				chart.data["2dxtraSet"] === null &&
+				chart.difficulty !== "LEGGENDARIA" &&
+				chart.difficulty !== "BEGINNER"
+			) {
+				chart.versions.push("inf");
+			}
 		}
 	}
 

a9d6193ced9f07dc0149ce929f48d1eceadfaade
saintnoodle <hi@noodle.moe>
2024-04-09 01:38:11 +0000

    remove mui/material

--- client/src/components/layout/screens/SplashScreen.tsx
+++ client/src/components/layout/screens/SplashScreen.tsx
@@ -1,5 +1,5 @@
-import { CircularProgress } from "@mui/material";
 import React from "react";
+import Loading from "components/util/Loading";
 import SplashImage from "../misc/SplashImage";
 
 export function SplashScreen({ broke }: { broke: string }) {
@@ -9,7 +9,7 @@ export function SplashScreen({ broke }: { broke: string }) {
 			className="bg-body position-fixed inset-0 d-flex flex-column justify-content-center align-items-center"
 		>
 			<SplashImage />
-			{!broke && <CircularProgress />}
+			{!broke && <Loading />}
 			{broke && <p className="mt-4">{broke}</p>}
 		</div>
 	);

2cc5513a5cc122600c208446571217f0fd7db561
zkldi <20380519+zkldi@users.noreply.github.com>
2024-04-25 02:54:53 +0100

    fix: remove debug logging

--- github-bot/src/main.ts
+++ github-bot/src/main.ts
@@ -95,9 +95,6 @@ ${mkSeedDiffViewMsg(
 app.webhooks.on(["issue_comment.created"], async ({ octokit, payload }) => {
 	const body = payload.comment.body.trim();
 
-	console.log(`debug, temp: Issue comment created with ${body}`);
-	console.dir(payload);
-
 	if (body.startsWith("+bot")) {
 		const cmd = body.split(" ")[1]?.replace(/[^a-z]/u, "");
 

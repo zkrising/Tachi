7afe4ccc828049cf027d8976c77e101268042920
zkldi <20380519+zkldi@users.noreply.github.com>
2024-04-25 01:11:42 +0100

    clean up, mock out for tests

--- server/src/lib/score-import/import-types/common/api-kai/sdvx/parser.test.ts
+++ server/src/lib/score-import/import-types/common/api-kai/sdvx/parser.test.ts
@@ -13,6 +13,8 @@ const fakeAuth = {
 
 const logger = CreateLogCtx(__filename);
 
+const NO_REAUTH = () => Promise.resolve("");
+
 t.test("#ParseKaiSDVX", (t) => {
 	t.beforeEach(ResetDBState);
 
@@ -32,7 +34,7 @@ t.test("#ParseKaiSDVX", (t) => {
 			},
 		});
 
-		const res = await ParseKaiSDVX("FLO", fakeAuth, logger, mockFloAPI);
+		const res = await ParseKaiSDVX("FLO", fakeAuth, logger, mockFloAPI, NO_REAUTH);
 
 		t.equal(res.game, "sdvx");
 		t.strictSame(res.context, { service: "FLO" });
@@ -64,7 +66,7 @@ t.test("#ParseKaiSDVX", (t) => {
 			},
 		});
 
-		const res = await ParseKaiSDVX("EAG", fakeAuth, logger, mockEagAPI);
+		const res = await ParseKaiSDVX("EAG", fakeAuth, logger, mockEagAPI, NO_REAUTH);
 
 		t.equal(res.game, "sdvx");
 		t.strictSame(res.context, { service: "EAG" });

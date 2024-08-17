e18197d0d01d2a8dec5722e40b1e5199926a0902
zkldi <20380519+zkldi@users.noreply.github.com>
2024-08-17 15:01:24 +0000

    feat: vendor node-tap

---
+++ vendor/tap/test/test.mjs
@@ -0,0 +1,25 @@
+import * as module from 'module'
+const { createRequire } = module
+
+import * as tap from 'tap'
+
+if (typeof createRequire !== 'function') {
+  console.log(`TAP version 13
+1..0 # SKIP - no createRequire function available ${process.version}
+`)
+  process.exit(0)
+}
+
+const require = createRequire(import.meta.url);
+
+const cjs = require('tap');
+
+const t = cjs
+
+t.test('tap', async t => {
+  t.matchSnapshot(Object.keys(tap).sort())
+
+  for (const key of Object.keys(tap)) {
+    t.equal(tap[key], key === 'default' ? cjs : cjs[key], key)
+  }
+})

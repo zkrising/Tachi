zkrising/Tachi

commit c254b69d40ff3828e938e7fb3038d5058a00c45d

feat: cg import for jubeat (#1261)
* feat(score-import): Initial support for jubeat imports from CG

* feat(client): enable jubeat imports from cg through dashboard

* feat(api-cg/jb): further implementation of jubeat import from cg

* feat(api-cg/jb): create test

* fix(api-cg/jb): test on testingdb only has one chart

* refactor(api-cg/jb): cleanup converter

* fix(api-cg/jb): fix test mkOutput usage

* fix(api-cg/jb): validation of the int to enum conversion from cg

* feat(client): frontent jubeat import routes for cg

* feat(bot): add jubeat cg import command

server/src/lib/score-import/import-types/parsers.ts
@@ -11,6 +11,7 @@ import {
 	ParseCGDevMuseca,
 	ParseCGDevPopn,
 	ParseCGDevSDVX,
+	ParseCGDevJubeat,
 	ParseCGGanMuseca,
 	ParseCGGanPopn,
 	ParseCGGanSDVX,
@@ -55,18 +56,17 @@ export const Parsers = {
 	"api/cg-dev-sdvx": ParseCGDevSDVX,
 	"api/cg-dev-popn": ParseCGDevPopn,
 	"api/cg-dev-museca": ParseCGDevMuseca,
-
-	// temporarily disabled as no musicRate is provided.
-	// "api/cg-dev-jubeat": ParseCGDevJubeat,
-	// "api/cg-prod-jubeat": ParseCGProdJubeat,
+	"api/cg-dev-jubeat": ParseCGDevJubeat,
 
 	"api/cg-nag-sdvx": ParseCGNagSDVX,
 	"api/cg-nag-popn": ParseCGNagPopn,
 	"api/cg-nag-museca": ParseCGNagMuseca,
+	"api/cg-nag-jubeat": ParseCGDevJubeat,
 
 	"api/cg-gan-sdvx": ParseCGGanSDVX,
 	"api/cg-gan-popn": ParseCGGanPopn,
 	"api/cg-gan-museca": ParseCGGanMuseca,
+	"api/cg-gan-jubeat": ParseCGDevJubeat,
 
 	"api/myt-chunithm": ParseMytChunithm,
 	"api/myt-maimaidx": ParseMytMaimaiDx,

server/src/lib/score-import/import-types/common/api-cg/parsers.ts
@@ -35,6 +35,7 @@ const PR_CG_JUBEAT = {
 	goodCount: p.isPositiveInteger,
 	poorCount: p.isPositiveInteger,
 	missCount: p.isPositiveInteger,
+	musicRate: p.isBoundedInteger(0, 120_0),
 	dateTime: "string",
 };
 

bot/src/slashCommands/commands/sync.ts
@@ -14,6 +14,7 @@ const choices: Array<[string, string]> = (
 		["CG DEV SDVX", "api/cg-dev-sdvx"],
 		["CG DEV MUSECA", "api/cg-dev-museca"],
 		["CG DEV Pop'n", "api/cg-dev-popn"],
+		["CG DEV Jubeat", "api/cg-dev-jubeat"],
 		["CG SDVX", "api/cg-prod-sdvx"],
 		["CG MUSECA", "api/cg-prod-museca"],
 		["CG Pop'n", "api/cg-prod-popn"],

client/src/app/pages/dashboard/import/ImportPage.tsx
@@ -360,12 +360,13 @@ function ImportInfoDisplayer({ game }: { game: Game }) {
 			/>,
 			<ImportTypeInfoCard key="api/myt-ongeki" importType="api/myt-ongeki" />
 		);
-	} // else if (game === "jubeat") {
-	// 	Content.unshift(
-	// 		<ImportTypeInfoCard key="api/cg-dev-jubeat" importType="api/cg-dev-jubeat" />,
-	// 		<ImportTypeInfoCard key="api/cg-prod-jubeat" importType="api/cg-prod-jubeat" />
-	// 	);
-	// }
+	} else if (game === "jubeat") {
+		Content.unshift(
+			<ImportTypeInfoCard key="api/cg-dev-jubeat" importType="api/cg-dev-jubeat" />,
+			<ImportTypeInfoCard key="api/cg-gan-jubeat" importType="api/cg-gan-jubeat" />,
+			<ImportTypeInfoCard key="api/cg-nag-jubeat" importType="api/cg-nag-jubeat" />
+		);
+	}
 
 	return (
 		<>
@@ -611,6 +612,36 @@ function ImportTypeInfoCard({
 					desc="Pull your MUSECA scores from the NAG Network."
 					moreInfo="Note: All networks are reduced to their first three letters for anonymity reasons."
 					key="cg-nag-museca"
+				/>
+			);
+		case "api/cg-dev-jubeat":
+			return (
+				<ImportInfoCard
+					name="CG Dev Integration"
+					href="cg-dev-jubeat"
+					desc="Pull your Jubeat scores from the CG Dev Network."
+					moreInfo="Note: All networks are reduced to their first three letters for anonymity reasons."
+					key="cg-dev-jubeat"
+				/>
+			);
+		case "api/cg-nag-jubeat":
+			return (
+				<ImportInfoCard
+					name="CG NAG Integration"
+					href="cg-nag-jubeat"
+					desc="Pull your Jubeat scores from the NAG Network."
+					moreInfo="Note: All networks are reduced to their first three letters for anonymity reasons."
+					key="cg-nag-jubeat"
+				/>
+			);
+		case "api/cg-gan-jubeat":
+			return (
+				<ImportInfoCard
+					name="CG GAN Integration"
+					href="cg-gan-jubeat"
+					desc="Pull your Jubeat scores from the GAN Network."
+					moreInfo="Note: All networks are reduced to their first three letters for anonymity reasons."
+					key="cg-gan-jubeat"
 				/>
 			);
 		case "api/myt-chunithm":

client/src/app/routes/ImportRoutes.tsx
@@ -199,6 +199,9 @@ export default function ImportRoutes() {
 							<Route exact path="/import/cg-dev-museca">
 								<CGIntegrationPage cgType="dev" game="museca" />
 							</Route>
+							<Route exact path="/import/cg-dev-jubeat">
+								<CGIntegrationPage cgType="dev" game="jubeat" />
+							</Route>
 
 							<Route exact path="/import/cg-nag-sdvx">
 								<CGIntegrationPage cgType="nag" game="sdvx" />
@@ -209,6 +212,9 @@ export default function ImportRoutes() {
 							<Route exact path="/import/cg-nag-museca">
 								<CGIntegrationPage cgType="nag" game="museca" />
 							</Route>
+							<Route exact path="/import/cg-nag-jubeat">
+								<CGIntegrationPage cgType="nag" game="jubeat" />
+							</Route>
 
 							<Route exact path="/import/cg-gan-sdvx">
 								<CGIntegrationPage cgType="gan" game="sdvx" />
@@ -218,6 +224,9 @@ export default function ImportRoutes() {
 							</Route>
 							<Route exact path="/import/cg-gan-museca">
 								<CGIntegrationPage cgType="gan" game="museca" />
+							</Route>
+							<Route exact path="/import/cg-gan-jubeat">
+								<CGIntegrationPage cgType="gan" game="jubeat" />
 							</Route>
 
 							<Route exact path="/import/myt-chunithm">

client/src/components/imports/CGIntegrationPage.tsx
@@ -17,7 +17,7 @@ import ImportStateRenderer from "./ImportStateRenderer";
 
 interface Props {
 	cgType: "dev" | "gan" | "nag";
-	game: "sdvx" | "popn" | "museca";
+	game: "sdvx" | "popn" | "museca" | "jubeat";
 }
 
 export default function CGIntegrationPage({ cgType, game }: Props) {

common/src/constants/import-types.ts
@@ -38,12 +38,15 @@ const API_IMPORT_TYPES: Record<APIImportTypes, true> = {
 	"api/myt-maimaidx": true,
 	"api/myt-ongeki": true,
 	"api/myt-wacca": true,
+	"api/cg-dev-jubeat": true,
 	"api/cg-dev-museca": true,
 	"api/cg-dev-popn": true,
 	"api/cg-dev-sdvx": true,
+	"api/cg-nag-jubeat": true,
 	"api/cg-nag-museca": true,
 	"api/cg-nag-popn": true,
 	"api/cg-nag-sdvx": true,
+	"api/cg-gan-jubeat": true,
 	"api/cg-gan-museca": true,
 	"api/cg-gan-popn": true,
 	"api/cg-gan-sdvx": true,

common/src/types/import-types.ts
@@ -21,7 +21,7 @@ export type APIImportTypes =
 	// cg has dev and prod supported
 	// with four games.
 	// this typescript feature for stringliteral expansion is kinda neat.
-	| `api/cg-${"dev" | "gan" | "nag"}-${"museca" | "popn" | "sdvx"}`;
+	| `api/cg-${"dev" | "gan" | "nag"}-${"jubeat" | "museca" | "popn" | "sdvx"}`;
 
 export type IRImportTypes =
 	| "ir/barbatos"

commit 4d13200bce378fd0fd0b27504de89f876ef84441

Fix NAG/GAN jb import using DEV url
Fix a bug introduced in https://github.com/zkrising/Tachi/commit/c254b69d40ff3828e938e7fb3038d5058a00c45d where importing from GAN or NAG would use DEV url instead

server/src/lib/score-import/import-types/parsers.ts
@@ -15,9 +15,11 @@ import {
 	ParseCGGanMuseca,
 	ParseCGGanPopn,
 	ParseCGGanSDVX,
+	ParseCGGanJubeat,
 	ParseCGNagMuseca,
 	ParseCGNagPopn,
 	ParseCGNagSDVX,
+	ParseCGNagJubeat,
 } from "./common/api-cg/parsers";
 import ParseBatchManual from "./file/batch-manual/parser";
 import ParseEamusementIIDXCSV from "./file/eamusement-iidx-csv/parser";
@@ -61,12 +63,12 @@ export const Parsers = {
 	"api/cg-nag-sdvx": ParseCGNagSDVX,
 	"api/cg-nag-popn": ParseCGNagPopn,
 	"api/cg-nag-museca": ParseCGNagMuseca,
-	"api/cg-nag-jubeat": ParseCGDevJubeat,
+	"api/cg-nag-jubeat": ParseCGNagJubeat,
 
 	"api/cg-gan-sdvx": ParseCGGanSDVX,
 	"api/cg-gan-popn": ParseCGGanPopn,
 	"api/cg-gan-museca": ParseCGGanMuseca,
-	"api/cg-gan-jubeat": ParseCGDevJubeat,
+	"api/cg-gan-jubeat": ParseCGGanJubeat,
 
 	"api/myt-chunithm": ParseMytChunithm,
 	"api/myt-maimaidx": ParseMytMaimaiDx,
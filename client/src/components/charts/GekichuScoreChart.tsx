3e4870958f696c881df864f4da17899ecd70b15c
nairobi <glimglam@protonmail.com>
2025-05-05 14:02:54 +0200

    fix(client): shrink graphs when necessary

--- client/src/components/charts/GekichuScoreChart.tsx
+++ client/src/components/charts/GekichuScoreChart.tsx
@@ -170,6 +170,8 @@ export default function GekichuScoreChart({
 
 	const gradientId = type === "Score" ? difficulty : type;
 
+	data.length = Math.min(data.length, duration + 1);
+
 	const commonProps: Omit<LineSvgProps, "data"> = {
 		margin: { top: 30, bottom: 50, left: 50, right: 50 },
 		enableGridX: false,

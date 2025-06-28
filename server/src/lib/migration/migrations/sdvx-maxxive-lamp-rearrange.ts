b3485ee2c7cf763d61422e150bd5c95a168ea965
Pinapelz <donaldshan1@outlook.com>
2025-06-28 08:19:52 -0700

    SDVX: add MAXXIVE CLEAR lamp (#1288)
    * sdvx: add ac MAXXIVE CLEAR lamp
    
    * seperate USC lamps from SDVX for tests
    
    * Add maxxive in converter for api-cg/sdvx
    
    * feat: add migration
    
    * fix: pre-empt service support
    
    * lint(fix)
    
    * fix: my typescript tooling stopped working
    
    * fix: test
    
    ---------
    
    Co-authored-by: blobdash <fruityenloops@gmail.com>
    Co-authored-by: zkldi <20380519+zkldi@users.noreply.github.com>

---
+++ server/src/lib/migration/migrations/sdvx-maxxive-lamp-rearrange.ts
@@ -0,0 +1,58 @@
+import db from "external/mongo/db";
+import type { Migration } from "utils/types";
+
+const migration: Migration = {
+	id: "sdvx-maxxive-lamp-rearrange",
+	up: async () => {
+		await db.scores.update(
+			{
+				game: "sdvx",
+				"scoreData.lamp": "ULTIMATE CHAIN",
+			},
+			{
+				$set: {
+					"scoreData.enumIndexes.lamp": 4,
+				},
+			}
+		);
+
+		await db.scores.update(
+			{
+				game: "sdvx",
+				"scoreData.lamp": "PERFECT ULTIMATE CHAIN",
+			},
+			{
+				$set: {
+					"scoreData.enumIndexes.lamp": 5,
+				},
+			}
+		);
+	},
+	down: async () => {
+		await db.scores.update(
+			{
+				game: "sdvx",
+				"scoreData.lamp": "ULTIMATE CHAIN",
+			},
+			{
+				$set: {
+					"scoreData.enumIndexes.lamp": 3,
+				},
+			}
+		);
+
+		await db.scores.update(
+			{
+				game: "sdvx",
+				"scoreData.lamp": "PERFECT ULTIMATE CHAIN",
+			},
+			{
+				$set: {
+					"scoreData.enumIndexes.lamp": 4,
+				},
+			}
+		);
+	},
+};
+
+export default migration;

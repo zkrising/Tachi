42d442034f0305db90e6d82bd6c98a8220bce16d
nairobi <61069237+nyairobi@users.noreply.github.com>
2025-07-14 17:02:43 +0000

    fix(client): invalid array length on graph truncation (#1325)

--- client/src/components/charts/GekichumaiScoreChart.tsx
+++ client/src/components/charts/GekichumaiScoreChart.tsx
@@ -211,7 +211,7 @@ export default function GekichumaiScoreChart({
 
 	const gradientId = type === "Score" ? difficulty : type;
 
-	data[0].data.length = Math.min(data[0].data.length, duration + 1);
+	data[0].data.length = Math.min(data[0].data.length, Math.ceil(duration));
 
 	const commonProps: Omit<LineSvgProps, "data"> = {
 		margin: { top: 30, bottom: 50, left: 50, right: 50 },

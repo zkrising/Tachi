7027cba8a0d6804c06cb9ee3d433c38858bea216
HutchyBen <38333275+HutchyBen@users.noreply.github.com>
2025-07-21 01:57:30 +0000

    fix: use 1e-9 for more sanity

--- server/src/game-implementations/games/ddr.ts
+++ server/src/game-implementations/games/ddr.ts
@@ -80,7 +80,7 @@ export const DDR_SCORE_VALIDATORS: Array<ScoreValidator<"ddr:DP" | "ddr:SP">> = 
 					(stepScore - 10) * PERFECT +
 					((stepScore * 3) / 5 - 10) * GREAT +
 					(stepScore / 5 - 10) * GOOD +
-					1e-6) / // Floating point inaccuracy can cause 999959.9999999 which gets incorrectly floored
+					1e-9) / // Floating point inaccuracy can cause 999959.9999999 which gets incorrectly floored
 					10
 			) * 10;
 

fa273225aa70838a91f9e18d3c2772abe282326c
zkldi <20380519+zkldi@users.noreply.github.com>
2025-08-31 12:55:59 +0000

    feat: stellaverse endpoint

--- server/src/server/router/api/v1/users/_userID/games/@gameSpecificRoutes/bms/router.ts
+++ server/src/server/router/api/v1/users/_userID/games/@gameSpecificRoutes/bms/router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import db from "external/mongo/db";
 import {
 	CUSTOM_TACHI_BMS_TABLES,
 	HandleBMSTableBodyRequest,
@@ -6,7 +7,7 @@ import {
 	HandleBMSTableHeaderRequest,
 } from "lib/game-specific/custom-bms-tables";
 import { ValidatePlaytypeFromParamFor } from "server/router/api/v1/games/_game/_playtype/middleware";
-import { AssignToReqTachiData, GetTachiData } from "utils/req-tachi-data";
+import { AssignToReqTachiData, GetTachiData, GetUGPT, GetUser } from "utils/req-tachi-data";
 import type { RequestHandler } from "express";
 
 const router: Router = Router({ mergeParams: true });
@@ -94,6 +95,74 @@ router.get(
 
 		// This handles returning a response for us.
 		return HandleBMSTableBodyRequest(customTable, req, res);
+	}
+);
+
+const MD5_CHECKSUM_LENGTH = "60b725f10c9c85c70d97880dfe8191b3".length;
+const SHA256_CHECKSUM_LENGTH = "87428fc522803d31065e7bce3cf03fe475096631e5e07bbd7a0fde60c4cf25c7"
+	.length;
+
+/**
+ * Get this user's best chart on the given chart MD5 or SHA256.
+ *
+ * @name GET /api/v1/users/:userID/games/bms/:playtype/best-score/:checksum
+ */
+router.get(
+	"/:playtype/best-score/:checksum",
+	ValidatePlaytypeFromParamFor("bms"),
+	async (req, res) => {
+		const user = GetUser(req);
+
+		if (!req.params.checksum) {
+			return res.status(400).json({ success: false, description: "No checksum provided." });
+		}
+
+		const checksum = req.params.checksum.toLowerCase();
+
+		let query = {};
+
+		if (!/^[0-9a-f]+$/u.exec(checksum)) {
+			return res.status(400).json({
+				success: false,
+				description: "Invalid checksum (Was not a MD5 or SHA256 checksum).",
+			});
+		}
+
+		if (checksum.length === MD5_CHECKSUM_LENGTH) {
+			query = {
+				"data.hashMD5": checksum,
+			};
+		} else if (checksum.length === SHA256_CHECKSUM_LENGTH) {
+			query = {
+				"data.hashSHA256": checksum,
+			};
+		} else {
+			return res.status(400).json({
+				success: false,
+				description: "Invalid checksum length (Was not a MD5 or SHA256 checksum).",
+			});
+		}
+
+		const chart = await db.charts.bms.findOne({
+			...query,
+		});
+
+		if (!chart) {
+			return res
+				.status(404)
+				.json({ success: false, description: "No chart found with the given checksum." });
+		}
+
+		const pb = await db["personal-bests"].findOne({
+			game: "bms",
+			playtype: chart.playtype,
+			userID: user.id,
+			chartID: chart.chartID,
+		});
+
+		const description = pb ? "Best score found." : "Player has not played this chart.";
+
+		return res.status(200).json({ success: true, description, body: pb });
 	}
 );
 
